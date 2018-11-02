54ab9a61608941d57432afc2656f4c9a37f50ecf
Christy Rollinson <crollinson@gmail.com>
2018-11-02 13:14:31 -0500

    Add checks of bias correction
    Adding checks to make sure hte bias correction step left us with sane met vars.  Spoiler alert for next commit: It didn't!  Looks like bias-correction cannot accomodate subdaily met yet :frowning:

--- ForestryTest_US-WCr/0_setup/2_Met_Bias_Correct.R
+++ ForestryTest_US-WCr/0_setup/2_Met_Bias_Correct.R
@@ -54,10 +54,10 @@ vers=".v2"
 site.lat  =  45.805822 # 45°48′21″N
 site.lon  = -90.079722 # 90°04′47″W
 
-ens=1
+ens=1:3
 n.ens=length(ens)
-# ens.mems=str_pad(ens, 3, "left", pad=0)
-ens.mems="bias-corrected"
+ens.mems=stringr::str_pad(ens, 3, "left", pad=0)
+# ens.mems="bias-corrected"
 
 # Set up the appropriate seeds to use when adding ensembles
 set.seed(1159)
@@ -112,8 +112,175 @@ debias.met.regression(train.data=met.out$dat.train, source.data=met.out$dat.sour
 
 
 # -----------------------------------
+# Double check the downscaling
+# -----------------------------------
+library(ggplot2)
+vars.CF <- c("air_temperature_minimum", "air_temperature_maximum", "precipitation_flux", "surface_downwelling_shortwave_flux_in_air", "surface_downwelling_longwave_flux_in_air", "air_pressure", "specific_humidity", "wind_speed")
+vars.short <- c("tair", "precip", "swdown", "lwdown", "press", "qair", "wind")
+
+# -------------------
+# Raw data
+# -------------------
+met.out <- align.met(train.path=path.nldas, source.path=path.GCM, yrs.train=NULL, yrs.source=NULL, n.ens=n.ens, seed=201811, pair.mems = FALSE, mems.train=paste(ens.ID, ens.mems, sep="_"))
+
+met.raw <- data.frame(met.out$dat.train$time)
+met.raw$dataset <- "NLDAS"
+met.raw$tair <- met.out$dat.train$air_temperature[,1]
+met.raw$precip   <- met.out$dat.train$precipitation_flux[,1]
+met.raw$swdown   <- met.out$dat.train$surface_downwelling_shortwave_flux_in_air[,1]
+met.raw$lwdown   <- met.out$dat.train$surface_downwelling_longwave_flux_in_air[,1]
+met.raw$press    <- met.out$dat.train$air_pressure[,1]
+met.raw$qair     <- met.out$dat.train$specific_humidity[,1]
+met.raw$wind     <- met.out$dat.train$wind_speed[,1]
 
 
+met.tmp <- data.frame(met.out$dat.source$time)
+met.tmp$dataset <- ens.ID
+met.tmp$tair <- met.out$dat.source$air_temperature[,1]
+met.tmp$precip   <- met.out$dat.source$precipitation_flux[,1]
+met.tmp$swdown   <- met.out$dat.source$surface_downwelling_shortwave_flux_in_air[,1]
+met.tmp$lwdown   <- met.out$dat.source$surface_downwelling_longwave_flux_in_air[,1]
+met.tmp$press    <- met.out$dat.source$air_pressure[,1]
+met.tmp$qair     <- met.out$dat.source$specific_humidity[,1]
+met.tmp$wind     <- sqrt(met.out$dat.source$eastward_wind[,1]^2 + met.out$dat.source$northward_wind[,1]^2)
+
+met.raw <- rbind(met.raw, met.tmp)
+
+# Aggregate to look at annual means
+met.raw.yr1 <- aggregate(met.raw[,vars.short], by=met.raw[,c("Year", "dataset")], FUN=mean)
+met.raw.yr1$dataset2 <- as.factor(met.raw.yr1$dataset)
+for(i in 1:nrow(met.raw.yr1)){
+  met.raw.yr1[i,"dataset"] <- stringr::str_split(met.raw.yr1[i,"dataset2"], "[.]")[[1]][1]
+}
+met.raw.yr1$dataset <- as.factor(met.raw.yr1$dataset)
+summary(met.raw.yr1)
+
+met.raw.yr <- stack(met.raw.yr1[,vars.short])
+names(met.raw.yr) <- c("raw", "met.var")
+met.raw.yr[,c("Year", "dataset", "dataset2")] <- met.raw.yr1[,c("Year", "dataset", "dataset2")]
+summary(met.raw.yr)
+
+
+library(ggplot2)
+met.raw.doy1 <- aggregate(met.raw[,vars.short], by=met.raw[,c("DOY", "dataset")], FUN=mean, na.rm=T)
+met.raw.doy1$dataset2 <- as.factor(met.raw.doy1$dataset)
+for(i in 1:nrow(met.raw.doy1)){
+  met.raw.doy1[i,"dataset"] <- stringr::str_split(met.raw.doy1[i,"dataset2"], "[.]")[[1]][1]
+}
+met.raw.doy1$dataset <- as.factor(met.raw.doy1$dataset)
+
+met.raw.doy <- stack(met.raw.doy1[,vars.short])
+names(met.raw.doy) <- c("raw", "met.var")
+met.raw.doy[,c("DOY", "dataset", "dataset2")] <- met.raw.doy1[,c("DOY", "dataset", "dataset2")]
+summary(met.raw.doy)
+
+
+summary(met.raw.doy1)
+# summary(met.bias.doy.mean)
+
+png(file.path(path.day.base, "Raw_Annual.png"), height=8, width=10, units="in", res=220)
+print(
+  ggplot(data=met.raw.yr[,]) + facet_wrap(~met.var, scales="free_y") +
+    geom_path(aes(x=Year, y=raw, color=dataset, group=dataset2), size=0.5) +
+    # geom_vline(xintercept=c(1850, 1901, 2016), linetype="dashed") +
+    scale_x_continuous(expand=c(0,0)) +
+    theme_bw()
+)
+dev.off()
+
+png(file.path(path.day.base, "Raw_DOY.png"), height=8, width=10, units="in", res=220)
+print(
+  ggplot(data=met.raw.doy[,]) + facet_wrap(~met.var, scales="free_y") +
+    geom_path(data=met.raw.doy[met.raw.doy$dataset=="NLDAS",], aes(x=DOY, y=raw), color="black", size=1) +
+    geom_path(data=met.raw.doy[met.raw.doy$dataset!="NLDAS",], aes(x=DOY, y=raw, color=dataset, group=dataset2), size=0.5) +
+    scale_x_continuous(expand=c(0,0)) +
+    theme_bw()
+)
+dev.off()
+# -------------------
+
+# -------------------
+# Bias-Corrected data
+# -------------------
+met.bias <- align.met(train.path=path.nldas, source.path=file.path(out.base, ens.ID), yrs.train=NULL, yrs.source=NULL, n.ens=n.ens, seed=201811, pair.mems = FALSE, mems.train=paste(ens.ID, ens.mems, sep="_"))
+
+met.raw <- data.frame(met.bias$dat.train$time)
+met.raw$dataset <- "NLDAS"
+met.raw$tair <- met.bias$dat.train$air_temperature[,1]
+met.raw$precip   <- met.bias$dat.train$precipitation_flux[,1]
+met.raw$swdown   <- met.bias$dat.train$surface_downwelling_shortwave_flux_in_air[,1]
+met.raw$lwdown   <- met.bias$dat.train$surface_downwelling_longwave_flux_in_air[,1]
+met.raw$press    <- met.bias$dat.train$air_pressure[,1]
+met.raw$qair     <- met.bias$dat.train$specific_humidity[,1]
+met.raw$wind     <- met.bias$dat.train$wind_speed[,1]
+
+
+met.tmp <- data.frame(met.bias$dat.source$time)
+met.tmp$dataset <- ens.ID
+met.tmp$tair <- met.bias$dat.source$air_temperature[,1]
+met.tmp$precip   <- met.bias$dat.source$precipitation_flux[,1]
+met.tmp$swdown   <- met.bias$dat.source$surface_downwelling_shortwave_flux_in_air[,1]
+met.tmp$lwdown   <- met.bias$dat.source$surface_downwelling_longwave_flux_in_air[,1]
+met.tmp$press    <- met.bias$dat.source$air_pressure[,1]
+met.tmp$qair     <- met.bias$dat.source$specific_humidity[,1]
+met.tmp$wind     <- sqrt(met.bias$dat.source$eastward_wind[,1]^2 + met.bias$dat.source$northward_wind[,1]^2)
+
+met.raw <- rbind(met.raw, met.tmp)
+
+# Aggregate to look at annual means
+met.raw.yr1 <- aggregate(met.raw[,vars.short], by=met.raw[,c("Year", "dataset")], FUN=mean)
+met.raw.yr1$dataset2 <- as.factor(met.raw.yr1$dataset)
+for(i in 1:nrow(met.raw.yr1)){
+  met.raw.yr1[i,"dataset"] <- stringr::str_split(met.raw.yr1[i,"dataset2"], "[.]")[[1]][1]
+}
+met.raw.yr1$dataset <- as.factor(met.raw.yr1$dataset)
+summary(met.raw.yr1)
+
+met.raw.yr <- stack(met.raw.yr1[,vars.short])
+names(met.raw.yr) <- c("raw", "met.var")
+met.raw.yr[,c("Year", "dataset", "dataset2")] <- met.raw.yr1[,c("Year", "dataset", "dataset2")]
+summary(met.raw.yr)
+
+
+library(ggplot2)
+met.raw.doy1 <- aggregate(met.raw[,vars.short], by=met.raw[,c("DOY", "dataset")], FUN=mean, na.rm=T)
+met.raw.doy1$dataset2 <- as.factor(met.raw.doy1$dataset)
+for(i in 1:nrow(met.raw.doy1)){
+  met.raw.doy1[i,"dataset"] <- stringr::str_split(met.raw.doy1[i,"dataset2"], "[.]")[[1]][1]
+}
+met.raw.doy1$dataset <- as.factor(met.raw.doy1$dataset)
+
+met.raw.doy <- stack(met.raw.doy1[,vars.short])
+names(met.raw.doy) <- c("raw", "met.var")
+met.raw.doy[,c("DOY", "dataset", "dataset2")] <- met.raw.doy1[,c("DOY", "dataset", "dataset2")]
+summary(met.raw.doy)
+
+
+summary(met.raw.doy1)
+# summary(met.bias.doy.mean)
+
+png(file.path(path.day.base, "Raw_Annual.png"), height=8, width=10, units="in", res=220)
+print(
+  ggplot(data=met.raw.yr[,]) + facet_wrap(~met.var, scales="free_y") +
+    geom_path(aes(x=Year, y=raw, color=dataset, group=dataset2), size=0.5) +
+    # geom_vline(xintercept=c(1850, 1901, 2016), linetype="dashed") +
+    scale_x_continuous(expand=c(0,0)) +
+    theme_bw()
+)
+dev.off()
+
+png(file.path(path.day.base, "Raw_DOY.png"), height=8, width=10, units="in", res=220)
+print(
+  ggplot(data=met.raw.doy[,]) + facet_wrap(~met.var, scales="free_y") +
+    geom_path(data=met.raw.doy[met.raw.doy$dataset=="NLDAS",], aes(x=DOY, y=raw), color="black", size=1) +
+    geom_path(data=met.raw.doy[met.raw.doy$dataset!="NLDAS",], aes(x=DOY, y=raw, color=dataset, group=dataset2), size=0.5) +
+    scale_x_continuous(expand=c(0,0)) +
+    theme_bw()
+)
+dev.off()
+# -------------------
+
+# -----------------------------------
 
 
 
