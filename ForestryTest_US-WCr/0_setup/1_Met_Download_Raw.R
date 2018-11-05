44fea267828f822a10be211962d425801219419c
Christy Rollinson <crollinson@gmail.com>
2018-11-05 16:22:54 -0600

    Run extraction; DAP is back to working and is faster!

--- ForestryTest_US-WCr/0_setup/1_Met_Download_Raw.R
+++ ForestryTest_US-WCr/0_setup/1_Met_Download_Raw.R
@@ -171,7 +171,7 @@ source("pecan_met_conversion/download.GFDL.R")
 ENS.all <- c("CM3", "ESM2M", "ESM2G")
 # CM3: https://www.gfdl.noaa.gov/coupled-physical-model-cm3/
 
-for(ENS in ENS.all){
+for(ENS in ENS.all[2:3]){
   download.GFDL(outfolder=file.path(path.out, site.name), 
                 start_date="2006-01-01", end_date="2100-12-31", 
                 lat.in=site.lat, lon.in=site.lon,
@@ -183,6 +183,6 @@ for(ENS in ENS.all){
                 lat.in=site.lat, lon.in=site.lon,
                 overwrite = FALSE, verbose = FALSE,
                 model = ENS, scenario = "rcp85", ensemble_member = "r1i1p1", add.co2=TRUE,
-                FTP=T)
+                FTP=FALSE)
 }
 # -------------------------------
