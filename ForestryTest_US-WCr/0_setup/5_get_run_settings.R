389bfbd1668f586077c4a1891e73dad89a56d7d2
crollinson <crollinson@gmail.com>
2018-11-11 11:44:19 -0600

    Download experimental design from google

---
+++ ForestryTest_US-WCr/0_setup/5_get_run_settings.R
@@ -0,0 +1,18 @@
+# Get Experimental Design from Google Drive and save it as a csv as a key for our ED runs
+library(googlesheets)
+
+# Register the Experimental Design Google sheet
+# Note: If doing this for the first time, it will open your browser and 
+#       you'll need to enter a key
+exp.design.wb <- gs_title("AGU Experimental Design")
+exp.design.wb # Prints all the metadata
+
+# Get the particular sheet & coerce it into a data frame rather than something special
+exp.design <- data.frame(gs_read(exp.design.wb, ws="Willow Creek"))
+summary(exp.design)
+
+# Not ready to do plantation yet, so we'll just do rows 1-8
+exp.design <- exp.design[1:8,]
+exp.design
+
+write.csv(exp.design, file="ExperimentalDesign_AGU2018.csv", na="", row.names=F, quote = F, fileEncoding="utf8")
