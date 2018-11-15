aa55cd74a7d0d4c85fc75e3e3e25d31156ac6860
Christy Rollinson <crollinson@gmail.com>
2018-11-14 19:12:47 -0600

    Updated Experiment: More sites, slightly different settings
    The goal will be to do at least on Ameriflux site from each of the major MANDIFORE ecoregions, but right  now, I can only generate initial conditoins for where the PEcAN VM already has them

--- ForestryTests_Ameriflux/0_setup/0_get_run_settings.R
+++ ForestryTests_Ameriflux/0_setup/0_get_run_settings.R
@@ -4,15 +4,15 @@ library(googlesheets)
 # Register the Experimental Design Google sheet
 # Note: If doing this for the first time, it will open your browser and 
 #       you'll need to enter a key
-exp.design.wb <- gs_title("AGU Experimental Design")
+exp.design.wb <- gs_title("Test_Climate_x_Mgmt_ExperimentalDesign")
 exp.design.wb # Prints all the metadata
 
 # Get the particular sheet & coerce it into a data frame rather than something special
-exp.design <- data.frame(gs_read(exp.design.wb, ws="Willow Creek"))
+exp.design <- data.frame(gs_read(exp.design.wb, ws="Experimental Design"))
 summary(exp.design)
 
 # Not ready to do plantation yet, so we'll just do rows 1-8
-exp.design <- exp.design[1:8,]
+# exp.design <- exp.design[1:8,]
 exp.design
 
-write.csv(exp.design, file="ExperimentalDesign_AGU2018.csv", na="", row.names=F, quote = F, fileEncoding="utf8")
+write.csv(exp.design, file="ExperimentalDesign_Test.csv", na="", row.names=F, quote = F, fileEncoding="utf8")
---
+++ ForestryTests_Ameriflux/0_setup/SoilCharacterstics.R
@@ -0,0 +1,38 @@
+# Pulling values from Web Soil Survey
+# - entered lat lon and then drew a box around the area
+# - used the surface layer with the weighted average aggregation method
+
+# Willow Creek (WCr); 45.805822, -90.079722
+wcr.areas <- c(0.041, 0.247, 0.230, 0.269, 0.007, 0.207)
+sum(wcr.areas)
+wcr.clay <- c(0.046, 0.041, 0.090, 0.090, 0.032, 0.110)
+wcr.sand <- c(0.793, 0.714, 0.628, 0.628, 0.893, 0.525)
+wcr.depth <- c(2.00, 0.84, 1.17, 1.17, 2.00, 1.14)
+sum(wcr.clay*wcr.areas) # Percent Clay
+sum(wcr.sand*wcr.areas) # Percent Sand
+sum(wcr.depth*wcr.areas) # Percent Sand
+
+# Harvard Forest (Ha1) 42.5378, -72.1715
+ha1.areas <- c(0.102, 0.109, 0.672, 0.118)
+ha1.clay <- c(0.95, 0.030, 0.030, 0.065)
+ha1.sand <- c(0.386, 0.575, 0.689, 0.571)
+ha1.depth <- c(2.00, 0.54, 0.84, 0.61)
+sum(ha1.clay*ha1.areas)
+sum(ha1.sand*ha1.areas)
+sum(ha1.depth*ha1.areas)
+
+
+# Niwot Ridge (NR1) 40.0329, -105.5464
+nr1.areas <- c(0.178, 0.744, 0.008, 0.070)
+nr1.clay <- c(0.136, 0.057, 0.099, 0.077)
+nr1.sand <- c(0.465, 0.659, 0.557, 0.662)
+nr1.depth <- c(0.71, 0.71, 0.71, 0.50)
+sum(nr1.areas)
+
+sum(nr1.clay*nr1.areas)
+sum(nr1.sand*nr1.areas)
+sum(nr1.depth*nr1.areas)
+
+
+# Austin Cary (SP1) 29.73807, -82.21877
+
