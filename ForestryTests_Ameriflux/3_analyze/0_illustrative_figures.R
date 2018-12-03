4677f807ddc86bbb28a01c8deb8ab7bafdac395a
Christy Rollinson <crollinson@gmail.com>
2018-12-03 17:12:10 -0600

    Update gitignore and dummy scripts for analysis

---
+++ ForestryTests_Ameriflux/3_analyze/0_illustrative_figures.R
@@ -0,0 +1,4 @@
+# Some generic, illustrative figures
+
+# 1. Map of Sites
+# 2. Example of Spatially implicit approach & management schemes using patch/cohort init file?
---
+++ ForestryTests_Ameriflux/3_analyze/2_initial_analyses.R
@@ -0,0 +1,1 @@
+# Doing some end-of-simulation comparisons
