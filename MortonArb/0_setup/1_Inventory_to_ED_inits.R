5b79e9e8a3ebc48b66427fc39dbaf70d4ce93465
Christy Rollinson <crollinson@gmail.com>
2019-09-10 12:54:06 -0500

    Beginnings of Morton Arb mgmt tests

---
+++ MortonArb/0_setup/1_Inventory_to_ED_inits.R
@@ -0,0 +1,8 @@
+# Script to convert 2018 East Woods survey data into ED initialization files
+# Management Units:
+#  - Hidden Lake
+#  - Annual Burn
+#  - No Burn
+#  - Other
+# 
+# Will also get Web Soil Survey data to capture any underlying differences in soil conditions factor in.
