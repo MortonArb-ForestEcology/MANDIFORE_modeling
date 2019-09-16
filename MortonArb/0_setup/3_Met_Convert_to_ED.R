9806d32d8cd73ed0725403d7b04d066f0d4776d2
Christy Rollinson <crollinson@gmail.com>
2019-09-16 16:13:42 -0500

    Initial stab at met conversion

--- MortonArb/0_setup/3_Met_Convert_to_ED.R
+++ MortonArb/0_setup/3_Met_Convert_to_ED.R
@@ -13,34 +13,25 @@ source(file.path(path.pecan, "models/ed/R/check_ed_metheader.R"))
 source("pecan_met_conversion/met2model.ED2.R")
 
 
-met.base="/mnt/data/crollinson/MANDIFORE_modeling/ForestryTests_Ameriflux/met_ed"
+met.base="/mnt/data/crollinson/MANDIFORE_modeling/MortonArb/met_ed"
 in.base="../met_raw/"
 outfolder="../met_ed/"
 
-# Open up the experimental design sheet
-# Load the experimental design table
-expdesign <- read.csv("ExperimentalDesign_Test.csv")
-summary(expdesign)
+SITE=site.name= "MortonArb"
+site.lat = 41.82
+site.lon = -88.04
 
-sites <- aggregate(expdesign[,c("lat", "lon")], by=list(expdesign$SiteName), mean)
-names(sites)[1] <- "SiteName"
+if(!dir.exists(outfolder)) dir.create(outfolder, recursive = T)
 
-sites <- sites[sites$SiteName %in% c("MORGANMONROE", "UMICHBIOLSTAT", "SYLVANIA"),]
+met.avail <- dir(file.path(in.base, SITE))
 
-for(SITE in sites$SiteName){
-  # met.base="~/Desktop/Research/MANDIFORE_modeling/ForestryTest_US-WCr/ED_MET/"
-  if(!dir.exists(outfolder)) dir.create(outfolder, recursive = T)
-  
-  met.avail <- dir(file.path(in.base, SITE))
-
-  # NEED TO FIX LEAP YEAR -- IT KEEPS SKIPPING!
-  for(GCM in met.avail){
-    met2model.ED2(in.path=file.path(in.base, SITE, GCM), 
-                  in.prefix=gsub("_", ".", GCM), 
-                  outfolder=file.path(outfolder, SITE, GCM), 
-                  header_folder = file.path(met.base, SITE, GCM),
-                  start_date="2006-01-01", 
-                  end_date="2100-12-31",
-                  leap_year = FALSE, overwrite=F)
-  }
+# NEED TO FIX LEAP YEAR -- IT KEEPS SKIPPING!
+for(GCM in met.avail){
+  met2model.ED2(in.path=file.path(in.base, SITE, GCM), 
+                in.prefix=gsub("_", ".", GCM), 
+                outfolder=file.path(outfolder, SITE, GCM), 
+                header_folder = file.path(met.base, SITE, GCM),
+                start_date="2006-01-01", 
+                end_date="2100-12-31",
+                leap_year = FALSE, overwrite=F)
 }
