1217db64881fac6bf391ef581059590eb3992592
Christy Rollinson <crollinson@gmail.com>
2020-10-19 07:34:46 -0500

    update eperiment queue

--- MortonArb/0_setup/5_Pull_ExperimentDesign.R
+++ MortonArb/0_setup/5_Pull_ExperimentDesign.R
@@ -3,12 +3,12 @@
 # Register the Experimental Design Google sheet
 # Note: If doing this for the first time, it will open your browser and 
 #       you'll need to enter a key
-exp.design.wb <- googlesheets::gs_title("MANDIFORE_TheMortonArboretum_Management_CaseStudy")
-exp.design.wb # Prints all the metadata
 
 # Get the particular sheet & coerce it into a data frame rather than something special
-exp.design <- data.frame(googlesheets::gs_read(exp.design.wb, ws="MortonArb_Experiment1"))
+exp.design <- googlesheets4::read_sheet(ss="1EROcBX_JI5828g6OLVcfUqz4wMu0cMMhCok0jSbRXD8", sheet="MortonArb_Experiment1", col_types = "c")
+exp.design <- data.frame(exp.design)
 summary(exp.design)
+head(exp.design)
 
 # Not ready to do plantation yet, so we'll just do rows 1-8
 # exp.design <- exp.design[1:8,]
