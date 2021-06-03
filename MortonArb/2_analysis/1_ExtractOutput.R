a2f96c7517c2bb10b8c358f04a285ef822c5978b
Christy Rollinson <crollinson@gmail.com>
2021-06-03 15:45:50 -0500

    Extraction updates
    1) add info on GCM, management, etc. to data frames; 2) extract patch & cohort data; 3) add weighted means for dbh & height to site data frame;
    
    STILL TO DO: add summary stats for patch (agb, height, dbh etc.)

--- MortonArb/2_analysis/1_ExtractOutput.R
+++ MortonArb/2_analysis/1_ExtractOutput.R
@@ -3,6 +3,8 @@ library(ggplot2)
 
 path.google <- "/Volumes/GoogleDrive/My Drive/MANDIFORE/MANDIFORE_CaseStudy_MortonArb"
 path.out <- "extract.v3"
+pfts.grass <- 5
+pfts.trees <- 6:11
 
 if(!dir.exists(path.out)) dir.create(path.out)
 
@@ -64,108 +66,171 @@ for(RUNID in runs.raw){
     # Setting up a dataframe of site level information
     # -- Note: We'll add to this with the patch and cohort level data, but I find it easier to work top-down
     # -- Note: _PY indicates "polygon", which means they're site level variables; ATM = "atmosphere", so those are drivers
-    site.list[[lab.now]] <- data.frame(year=yr.now,
-                                     month=mo.now,
+    # names(fnow$var)[grep("_PY", names(fnow$var))]
+    site.list[[lab.now]] <- data.frame(site=run.splt[1],
+                                       GCM=run.splt[3],
+                                       rcp=run.splt[4],
+                                       co2=run.splt[5],
+                                       Management=run.splt[6],
+                                       level="site",
+                                       year=yr.now,
+                                       month=mo.now,
+                                       
+                                       # Drivers
+                                       tair=ncdf4::ncvar_get(fnow, "MMEAN_ATM_TEMP_PY"), # K
+                                       precipf=ncdf4::ncvar_get(fnow, "MMEAN_PCPG_PY")*sec2mo[mo.now], #kg/m2/mo
+                                       lwdown=ncdf4::ncvar_get(fnow, "MMEAN_ATM_RLONG_PY"), # W/m2
+                                       swdown=ncdf4::ncvar_get(fnow, "MMEAN_ATM_RSHORT_PY"), # W/m2
+                                       qair=ncdf4::ncvar_get(fnow, "MMEAN_ATM_SHV_PY"), #kg/kg
+                                       psurf=ncdf4::ncvar_get(fnow, "MMEAN_ATM_PRSS_PY"), # Pa
+                                       wind=ncdf4::ncvar_get(fnow, "MMEAN_ATM_VELS_PY"), # m/s
+                                       CO2=ncdf4::ncvar_get(fnow, "MMEAN_ATM_TEMP_PY"), # umol/umol
+                                       
+                                       # Forest structure (trees only)
+                                       basal.area.tree = sum(ncdf4::ncvar_get(fnow, "BASAL_AREA_PY")[pfts.trees,]), # cm2/m2??
+                                       density.tree = sum(ncdf4::ncvar_get(fnow, "NPLANT_PY")[pfts.trees,]), # tree/m2??
+                                       
+                                       # Carbon Storage & Flux
+                                       agb=sum(ncdf4::ncvar_get(fnow, "AGB_PY")), # kg/m2??
+                                       gpp=ncdf4::ncvar_get(fnow, "MMEAN_GPP_PY")*yr2sec*sec2mo[mo.now], # kgC/m2/mo
+                                       npp=ncdf4::ncvar_get(fnow, "MMEAN_NPP_PY")*yr2sec*sec2mo[mo.now], # kgC/m2/mo
+                                       nee=-ncdf4::ncvar_get(fnow, "MMEAN_NEP_PY")*yr2sec*sec2mo[mo.now], # kgC/m2/mo
+                                       cwd=ncdf4::ncvar_get(fnow, "MMEAN_CWD_C_PY"), # kgC/m2
+                                       soil.c.fast=ncdf4::ncvar_get(fnow, "MMEAN_FAST_SOIL_C_PY"), # kgC/m2
+                                       soil.c.slow=ncdf4::ncvar_get(fnow, "MMEAN_SLOW_SOIL_C_PY"), # kgC/m2
+                                       soil.c.struc=ncdf4::ncvar_get(fnow, "MMEAN_STRUCT_SOIL_C_PY"), # kgC/m2
                                      
-                                     # Drivers
-                                     tair=ncdf4::ncvar_get(fnow, "MMEAN_ATM_TEMP_PY"), # K
-                                     precipf=ncdf4::ncvar_get(fnow, "MMEAN_PCPG_PY")*sec2mo[mo.now], #kg/m2/mo
-                                     lwdown=ncdf4::ncvar_get(fnow, "MMEAN_ATM_RLONG_PY"), # W/m2
-                                     swdown=ncdf4::ncvar_get(fnow, "MMEAN_ATM_RSHORT_PY"), # W/m2
-                                     qair=ncdf4::ncvar_get(fnow, "MMEAN_ATM_SHV_PY"), #kg/kg
-                                     psurf=ncdf4::ncvar_get(fnow, "MMEAN_ATM_PRSS_PY"), # Pa
-                                     wind=ncdf4::ncvar_get(fnow, "MMEAN_ATM_VELS_PY"), # m/s
-                                     CO2=ncdf4::ncvar_get(fnow, "MMEAN_ATM_TEMP_PY"), # umol/umol
-                                     
-                                     # Carbon Storage & Flux
-                                     agb=sum(ncdf4::ncvar_get(fnow, "AGB_PY")), # kg/m2??
-                                     basal.area = sum(ncdf4::ncvar_get(fnow, "BASAL_AREA_PY")), # cm2/m2??
-                                     gpp=ncdf4::ncvar_get(fnow, "MMEAN_GPP_PY")*yr2sec*sec2mo[mo.now], # kgC/m2/mo
-                                     npp=ncdf4::ncvar_get(fnow, "MMEAN_NPP_PY")*yr2sec*sec2mo[mo.now], # kgC/m2/mo
-                                     nee=-ncdf4::ncvar_get(fnow, "MMEAN_NEP_PY")*yr2sec*sec2mo[mo.now], # kgC/m2/mo
-                                     cwd=ncdf4::ncvar_get(fnow, "MMEAN_CWD_C_PY"), # kgC/m2
-                                     soil.c.fast=ncdf4::ncvar_get(fnow, "MMEAN_FAST_SOIL_C_PY"), # kgC/m2
-                                     soil.c.slow=ncdf4::ncvar_get(fnow, "MMEAN_SLOW_SOIL_C_PY"), # kgC/m2
-                                     soil.c.struc=ncdf4::ncvar_get(fnow, "MMEAN_STRUCT_SOIL_C_PY"), # kgC/m2
-                                     
-                                     # Energy Flux
-                                     albedo.short=ncdf4::ncvar_get(fnow, "MMEAN_ALBEDO_PY"),
-                                     par.ground=ncdf4::ncvar_get(fnow, "MMEAN_PAR_GND_PY"),
-                                     
-                                     # Soil, Hydrology & Water Flux
-                                     soil.moist.surf=ncdf4::ncvar_get(fnow, "MMEAN_SOIL_WATER_PY")[length(soil.depth)], # m3/m3
-                                     soil.moist.deep=ncdf4::ncvar_get(fnow, "MMEAN_SOIL_WATER_PY")[1], # m3/m3
-                                     soil.moistpot.surf=ncdf4::ncvar_get(fnow, "MMEAN_SOIL_MSTPOT_PY")[length(soil.depth)], # ???
-                                     soil.moistpot.deep=ncdf4::ncvar_get(fnow, "MMEAN_SOIL_MSTPOT_PY")[1], # ???
-                                     soil.temp.surf=ncdf4::ncvar_get(fnow, "MMEAN_SOIL_TEMP_PY")[length(soil.depth)], # K
-                                     soil.temp.deep=ncdf4::ncvar_get(fnow, "MMEAN_SOIL_TEMP_PY")[1], # K
-                                     runoff.surface=ncdf4::ncvar_get(fnow, "MMEAN_RUNOFF_PY")*sec2mo[mo.now], # kg/m2/mo??
-                                     runoff.subsurf=ncdf4::ncvar_get(fnow, "MMEAN_DRAINAGE_PY")*sec2mo[mo.now], # kg/m2/mo??
-                                     transp = ncdf4::ncvar_get(fnow, "MMEAN_TRANSP_PY")*sec2mo[mo.now], # kg/m2/mo
-                                     snow.depth=ncdf4::ncvar_get(fnow, "MMEAN_SFCW_DEPTH_PY"), # m
-                                     swe=ncdf4::ncvar_get(fnow, "MMEAN_SFCW_MASS_PY"), # kg/m2
-                          
-                          
-                                     # Energetics
-                                     albedo=ncdf4::ncvar_get(fnow, "MMEAN_ALBEDO_PY"),
-                                     
-                                     # Other
-                                     lai=sum(ncdf4::ncvar_get(fnow, "MMEAN_LAI_PY"))
-                                     )
-    
+                                       # Energy Flux
+                                       albedo.short=ncdf4::ncvar_get(fnow, "MMEAN_ALBEDO_PY"),
+                                       par.ground=ncdf4::ncvar_get(fnow, "MMEAN_PAR_GND_PY"),
+                                       
+                                       # Soil, Hydrology & Water Flux
+                                       soil.moist.surf=ncdf4::ncvar_get(fnow, "MMEAN_SOIL_WATER_PY")[length(soil.depth)], # m3/m3
+                                       soil.moist.deep=ncdf4::ncvar_get(fnow, "MMEAN_SOIL_WATER_PY")[1], # m3/m3
+                                       soil.moistpot.surf=ncdf4::ncvar_get(fnow, "MMEAN_SOIL_MSTPOT_PY")[length(soil.depth)], # ???
+                                       soil.moistpot.deep=ncdf4::ncvar_get(fnow, "MMEAN_SOIL_MSTPOT_PY")[1], # ???
+                                       soil.temp.surf=ncdf4::ncvar_get(fnow, "MMEAN_SOIL_TEMP_PY")[length(soil.depth)], # K
+                                       soil.temp.deep=ncdf4::ncvar_get(fnow, "MMEAN_SOIL_TEMP_PY")[1], # K
+                                       runoff.surface=ncdf4::ncvar_get(fnow, "MMEAN_RUNOFF_PY")*sec2mo[mo.now], # kg/m2/mo??
+                                       runoff.subsurf=ncdf4::ncvar_get(fnow, "MMEAN_DRAINAGE_PY")*sec2mo[mo.now], # kg/m2/mo??
+                                       transp = ncdf4::ncvar_get(fnow, "MMEAN_TRANSP_PY")*sec2mo[mo.now], # kg/m2/mo
+                                       snow.depth=ncdf4::ncvar_get(fnow, "MMEAN_SFCW_DEPTH_PY"), # m
+                                       swe=ncdf4::ncvar_get(fnow, "MMEAN_SFCW_MASS_PY"), # kg/m2
+                                       
+                                       # Energetics
+                                       albedo=ncdf4::ncvar_get(fnow, "MMEAN_ALBEDO_PY"),
+                                       
+                                       # Other
+                                       lai=sum(ncdf4::ncvar_get(fnow, "MMEAN_LAI_PY"))
+                                       )
+
     # this is a dataframe of "patch" level information.  Patches are analogous to forest "stands", but the number and identity of patches will change through time based on how similar the trees in different stands are.
     # Cohorts are _analogous_ to unique trees and make up a stand.
-    # df.pch <- data.frame(year=yr.now,
-    #                      month=mo.now,
-    #                      patch.start=ncdf4::ncvar_get(fnow, "PACO_ID"), # Something that links cohorts ot pathces; i _think_ which cohort row each patch starts on
-    #                      n.cohorts=ncdf4::ncvar_get(fnow, "PACO_N"), # How many cohorts are in a patch 
-    #                      patch.area = ncdf4::ncvar_get(fnow, "AREA"), # How big each patch is -- what percent of the area is taken up by forests or stands like each one?
-    #                      patch.age = ncdf4::ncvar_get(fnow, "AGE")) # How "old" a patch is -- time since last major disturbance.
-    # df.pch$patchID <- 1:nrow(df.pch)
-    # # summary(df.pch)
-    # 
+    pch.list[[lab.now]] <- data.frame(site=run.splt[1],
+                                      GCM=run.splt[3],
+                                      rcp=run.splt[4],
+                                      co2=run.splt[5],
+                                      Management=run.splt[6],
+                                      level="patches",
+                                      year=yr.now,
+                                      month=mo.now,
+                                      
+                                      # Patch-level data
+                                      age=ncdf4::ncvar_get(fnow, "AGE"),
+                                      area.patch=ncdf4::ncvar_get(fnow, "AREA"),
+                                      n.cohorts=ncdf4::ncvar_get(fnow, "PACO_N"),
+                                      patch.co.start=ncdf4::ncvar_get(fnow, "PACO_ID")
+                                      )
+    
+    pch.list[[lab.now]]$patchID <- 1:nrow(pch.list[[lab.now]])
+    
     # # This is the cohort data, which is not dissimilar for tree-level data, but is similar to the patch 
     # #   data in that "trees" of similar size and same type (PFT) and gives each one a density in the patch
     # #   PFT= plant functional type =~ "species" (but not really)
-    # df.co <- data.frame(year=yr.now,
-    #                     month=mo.now,
-    #                     PFT=as.factor(ncdf4::ncvar_get(fnow, "PFT")), # Which type of tree is this
-    #                     Density=ncdf4::ncvar_get(fnow, "NPLANT"), # What is the density for this cohort; (trees/m2)
-    #                     DBH=ncdf4::ncvar_get(fnow, "DBH"), # Diameter for this cohort (cm/tree)
-    #                     AGB=ncdf4::ncvar_get(fnow, "AGB_CO"), # Aboveground biomass for the cohort (kg/tree)
-    #                     BA=ncdf4::ncvar_get(fnow, "BA_CO"), # Basal area for the cohort (cm2/tree)
-    #                     Stress=ncdf4::ncvar_get(fnow, "CBR_BAR")) # This is a runnign carbon balance average for the tree; ranges from -1 to 1 I think; this might get us stress; computed from light- and water-induced stress
-    # # summary(df.co)
-    # 
+    co.list[[lab.now]] <- data.frame(site=run.splt[1],
+                                     GCM=run.splt[3],
+                                     rcp=run.splt[4],
+                                     co2=run.splt[5],
+                                     Management=run.splt[6],
+                                     level="cohorts",
+                                     year=yr.now,
+                                     month=mo.now,
+                                     
+                                     # Co-hort level data
+                                     pft=ncdf4::ncvar_get(fnow, "PFT"), # 5=grass; 9=early hard; 10=mid hard; 11=late hard
+                                     dbh=ncdf4::ncvar_get(fnow, "DBH"), # cm
+                                     ba=ncdf4::ncvar_get(fnow, "BA_CO"), # cm2/tree
+                                     carbs=ncdf4::ncvar_get(fnow, "CB")[13,], # Carbon Balance (carb reserves)? scale = 0(?) to 1; 1 = not stressed
+                                     stress = ncdf4::ncvar_get(fnow, "CBR_BAR"), # Running mean relative carbon balance based on water + light stress; scale = -1 to 1
+                                     height=ncdf4::ncvar_get(fnow, "HITE"), # meters
+                                     lai=ncdf4::ncvar_get(fnow, "LAI_CO"),
+                                     agb=ncdf4::ncvar_get(fnow, "AGB_CO"), # kgC/tree
+                                     dens.pch=ncdf4::ncvar_get(fnow, "NPLANT") #trees/m2 PER PATCH
+                                     )
+    
     ncdf4::nc_close(fnow) # Closing the connection to the netcdf file; this is important otherwise your computer will get confused
-    # 
-    # # Merging patch and cohort information
-    # 
-    # # step 1: add in patch area etc. to the cohorts so we can scale up 
-    # #   -- essentially need to do weighted averages to bring things to a forest-scale
-    # for(i in 1:nrow(df.pch)){
-    #   df.co[(df.pch$patch.start[i]-1):(df.pch$patch.start[i]-1+df.pch$n.cohorts[i]), c("patchID", "patch.area", "patch.age")] <- df.pch[i,c("patchID", "patch.area", "patch.age")]
-    # }
-    # # tail(df.co)
-    # 
-    # # This is the weighted density of the trees that will let us scale to the forest
-    # # Trees / patch * patch/forest = trees/forest in units trees/m2
-    # df.co$dens.wt <- df.co$Density*df.co$patch.area
-    # 
-    # # Using the density to created weighted values for certain groupings
-    # for(PFT in unique(df.co$PFT)){
-    #   df.co[df.co$PFT==PFT,"pft.wt"] <- df.co$dens.wt[df.co$PFT==PFT]/sum(df.co$dens.wt[df.co$PFT==PFT])
-    #   df.co[df.co$PFT==PFT & df.co$DBH>45,"pft.wt.g45"] <- df.co$dens.wt[df.co$PFT==PFT & df.co$DBH>45]/sum(df.co$dens.wt[df.co$PFT==PFT & df.co$DBH>45])
-    # }
-    # summary(df.co)
-    # 
-    # df.run <- rbind(df.run, df.co)
-      } # End File Loop
+  
+    # -----------------
+    # Merging patch and cohort information to help scale
+    # -----------------
+    # step 1: add in patch area etc. to the cohorts so we can scale up
+    #   -- essentially need to do weighted averages to bring things to a forest-scale
+    for(j in 1:nrow(pch.list[[lab.now]])){
+      rows.pch <- (pch.list[[lab.now]]$patch.co.start[j]):(pch.list[[lab.now]]$patch.co.start[j]-1+pch.list[[lab.now]]$n.cohorts[j])
+      
+      co.list[[lab.now]][rows.pch, c("patchID", "patch.area", "patch.age")] <- pch.list[[lab.now]][j,c("patchID", "area.patch", "age")]
+      
+      co.list[[lab.now]][rows.pch,"dens.wt.pch"] <- co.list[[lab.now]]$dens.pch[rows.pch]/sum(co.list[[lab.now]]$dens.pch[rows.pch])
+    }
+    # tail(co.list[[lab.now]])
+    
+    # This is the weighted density of the trees that will let us scale to the forest
+    # Trees / patch * patch/forest = trees/forest in units trees/m2
+    co.list[[lab.now]]$dens.site <- co.list[[lab.now]]$dens.pch*co.list[[lab.now]]$patch.area
+    co.list[[lab.now]]$dens.wt.site <- co.list[[lab.now]]$dens.site/sum(co.list[[lab.now]]$dens.site)
+    # -----------------
+
+    # -----------------
+    # Doing some aggregation of data to the patch level
+    # AGB, BAtree, mean/max/sd height, mean/max/sd DBH
+    # There's an R function called weighted.mean
+    # To get weighted sd:     
+    #      x=test.df$dbh
+    #      w=test.df$dens/sum(test.df$dens)
+    #      sqrt(sum(w * (x-weighted.mean(x, w))^2))
+    # -----------------
+    pch.list[[lab.now]][,c("height.max", "dbh.max")] <- aggregate(cbind(height, dbh) ~ patchID, data=co.list[[lab.now]], FUN=max)
+    
+    # -----------------
+    
+    # -----------------
+    # Doing some aggregation to the site level
+    # To get to the site level total from cohort level: sum(x * dens.wt)
+    # -----------------
+    # AGB, BAtree, mean/max/sd height, mean/max/sd DBH
+    tree.dbh <- co.list[[lab.now]]$dbh[co.list[[lab.now]]$pft %in% pfts.trees]
+    tree.hts <- co.list[[lab.now]]$height[co.list[[lab.now]]$pft %in% pfts.trees]
+    tree.wts <- co.list[[lab.now]]$dens.wt.site[co.list[[lab.now]]$pft %in% pfts.trees]
+    site.list[[lab.now]]$height.mean <- weighted.mean(tree.hts, tree.wts)
+    site.list[[lab.now]]$height.sd <- sqrt(sum(tree.wts * (tree.hts-site.list[[lab.now]]$height.mean)^2))
+
+    site.list[[lab.now]]$dbh.mean <- weighted.mean(tree.dbh, tree.wts)
+    site.list[[lab.now]]$dbh.sd <- sqrt(sum(tree.wts * (tree.dbh-site.list[[lab.now]]$dbh.mean)^2))
+    # -----------------
+    
+    
+    } # End File Loop
   
   site.df <- dplyr::bind_rows(site.list)
-  rm(site.list)
+  pch.df <- dplyr::bind_rows(pch.list)
+  co.df <- dplyr::bind_rows(co.list)
+  rm(site.list, pch.list, co.list)
   
-  write.csv(site.df, file.path(path.out, paste(RUNID, "Site.csv", sep="_")), row.names=F)
   write.csv(site.df, file.path(path.google, "output", paste(RUNID, "Site.csv", sep="_")), row.names=F) # Just save the site-level data on Google Too
+  write.csv(site.df, file.path(path.out, paste(RUNID, "Site.csv", sep="_")), row.names=F)
+  write.csv(pch.df, file.path(path.out, paste(RUNID, "Patch.csv", sep="_")), row.names=F)
+  write.csv(co.df, file.path(path.out, paste(RUNID, "Cohort.csv", sep="_")), row.names=F)
+  
+  
 } # End run loop
 
