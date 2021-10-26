4ba36ed2e19f9c33bc4800c12e2ee2b881b6018c
LucienFitzpatrick <lfitzpatrick@mortonarb.org>
2021-10-26 10:40:31 -0500

    New script for Subdail downloading and conversion
    Currently still having issues with precipitaiton flux matching the monthly aggregated ED output. still looking into it but pushing the current version

---
+++ MortonArb/2_analysis/0_Subdaily_Download.R
@@ -0,0 +1,112 @@
+#----------------------------------------------------------------------------------------------------------------------#
+# Script by : Lucien Fitzpatrick
+# Project: Mandifore Morton arb casee study
+# Purpose: This script pulls hourly weather data and aggregates it to daily.
+# Inputs: Met output found in MANDIFORE_modeling/MortonArb/met_raw.v3/subdaily_tdm/MortonArb
+# Outputs: A dataframe of summaraized daily weather data that has been aggregated from hourly data
+# Notes: 
+#----------------------------------------------------------------------------------------------------------------------#
+#Folder containing the data
+path.out = "../met_raw.v3"
+
+#Site identification
+site.name= "MortonArb"
+site.lat = 41.82
+site.lon = -88.04
+
+# Get a list of everything we have to work with
+dir.mods <- file.path(path.out, "subdaily_tdm", site.name)
+mods.raw <- dir(dir.mods)
+
+#Subsetting out GFDL-CM3_rcp45_r1i1p1 and GFDL-CM3_rcp85_r1i1p1 because they have a different format that is tricky
+mods.raw <- mods.raw[c(1:12,15:32)]
+
+#Setting the years and variables of interest
+yrs.all <- 2006:2099
+vars.all <- c("air_temperature", "precipitation_flux", "surface_downwelling_longwave_flux_in_air", "surface_downwelling_shortwave_flux_in_air", "air_pressure", "specific_humidity", "wind_speed")
+
+#Creating the list that will be populated
+mod.list <- list()
+#mods.raw <- mods.raw[1:3]
+for(MOD in mods.raw){
+  #Printing to make sure it's running
+  print(paste0("Processing Model: ", MOD))
+  
+  fmod <- dir(file.path(dir.mods, MOD))
+  
+  for(YR in yrs.all){
+    #Identifiying the proper file for this year
+    fnow <- fmod[grep(YR, fmod)]
+    
+    #Making sure we have the year in question
+    if(length(fnow)!=1) next
+    
+    ncT <- ncdf4::nc_open(file.path(dir.mods, MOD, fnow))
+    day.time <- ncdf4::ncvar_get(ncT, "time")
+    
+    #These documents uses a weird "Days since start" unit for hours giving values like (.0208, .0625) for hour 1 and 2
+    #Converting from days since year start to hours since year start
+    h.time <- 1:length(day.time)
+    
+    #Making the final Datetime object
+    final.time <- lubridate::hours(h.time) + as.POSIXct(paste0(YR, "-01-01 00:00:00"), format = "%Y-%m-%d %H:%M:%S")
+    
+    #Beginning the loop for every variable
+    for(VAR in vars.all){
+      #Checking for the windspeed exception
+      if(VAR %in% names(ncT$var)){
+        
+        #Building the list that contains the hourly values in nested lists for each model, year, and variable
+        mod.list[[paste(MOD, YR, VAR)]]$value <- ncdf4::ncvar_get(ncT, VAR)
+        mod.list[[paste(MOD, YR, VAR)]]$model <- MOD
+        mod.list[[paste(MOD, YR, VAR)]]$var <- VAR
+        mod.list[[paste(MOD, YR, VAR)]]$Date <- final.time 
+        mod.list[[paste(MOD, YR, VAR)]]$year <- YR
+        mod.list[[paste(MOD, YR, VAR)]]$yday <- lubridate::yday(final.time) 
+        
+      } 
+      #Special case for windspeed
+      
+      else if(VAR=="wind_speed" & "eastward_wind" %in% names(ncT$var)){
+        ew <- ncdf4::ncvar_get(ncT, "eastward_wind")
+        nw <- ncdf4::ncvar_get(ncT, "northward_wind")
+        
+        # Calculate wind speed from ew/nw using pythagorean theorem
+        mod.list[[paste(MOD, YR, VAR)]]$value <- sqrt(ew^2 + nw^2)
+        mod.list[[paste(MOD, YR, VAR)]]$model <- MOD
+        mod.list[[paste(MOD, YR, VAR)]]$var <- VAR
+        mod.list[[paste(MOD, YR, VAR)]]$Date<- final.time
+        mod.list[[paste(MOD, YR, VAR)]]$year <- YR
+        mod.list[[paste(MOD, YR, VAR)]]$yday <- lubridate::yday(final.time) 
+        
+      } 
+    } # end var loop
+    ncdf4::nc_close(ncT)
+  } # End file loop
+  
+} 
+# End model loop
+#Make into a dataframe
+mod.df <- dplyr::bind_rows(mod.list)
+
+#HERE is where I convert kg2/m2/sec to kg2/m2/hour
+mod.df$value <- ifelse(mod.df$var == "precipitation_flux", mod.df$value * 60 * 60, mod.df$value)
+
+#Creating a date object that is Daily (Removing hours)
+mod.df$Date <- as.Date(mod.df$Date)
+
+#Aggregating for each day
+day.df <- aggregate(value~var+Date+model, data =mod.df, FUN = mean)
+day.df$min <- aggregate(value~var+Date+model, data =mod.df, FUN = min)[,c("value")]
+day.df$max <- aggregate(value~var+Date+model, data =mod.df, FUN = max)[,c("value")]
+
+#Summing the precipitation converts from kg2/m2/hour to kg2/m2/day
+day.df$sum <- aggregate(value~var+Date+model, data =mod.df, FUN = sum)[,c("value")]
+colnames(day.df) <- c("var", "Date", "model", "mean", "min", "max", "sum")
+
+#Saving the dataframe
+write.csv(day.df, "../Aggregate_Weater_Daily.csv", row.names=F)
+
+#dat.precip <- day.df[day.df$var == "precipitation_flux",]
+#dat.temp <- day.df[day.df$var == "air_temperature",]
+
