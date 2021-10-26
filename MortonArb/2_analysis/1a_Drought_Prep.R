daeb15f9f13843bdd5b380c1478d6210a544729c
LucienFitzpatrick <lfitzpatrick@mortonarb.org>
2021-10-26 10:58:23 -0500

    Updates reflecting the new format of it's inputs

--- MortonArb/2_analysis/1a_Drought_Prep.R
+++ MortonArb/2_analysis/1a_Drought_Prep.R
@@ -18,8 +18,8 @@ runs.all <- read_bulk(directory = "output", extension = "Site.csv", header = TRU
 runs.all$Management <- car::recode(runs.all$Management, "'MgmtNone'='None'; 'MgmtGap'='Gap'; 'MgmtShelter'='Shelter'; 'MgmtUnder'='Under'")
 
 setwd("C:/Users/lucie/Documents/GitHub/MANDIFORE_modeling/MortonArb/2_analysis/")
-dat.met <- read.csv("../Full_Weather_Daily.csv")
-dat.met <- dat.met[dat.met$model %in% unique(runs.all$GCM),]
+dat.met <- read.csv("../Aggregate_Weater_Daily.csv")
+#dat.met <- dat.met[dat.met$model %in% unique(runs.all$GCM),]
 
 #Setting up droguht tracking
 
@@ -37,28 +37,16 @@ for(i in 1:nrow(dat.precip)){
     drought <- 0
   }
   dat.precip[i, "days_since_rain"] <- drought
-}
+} 
 
-
-#Adding a date and month object for matching
-for(i in 1:nrow(dat.precip)){
-  dat.precip[i, "Date"] <- as.Date(dat.precip[i, "yday"], origin = paste(dat.precip[i, "year"], "-01-01", sep = ""))
-  dat.precip[i, "month"] <- lubridate::month(dat.precip[i, "Date"])
-}
+dat.precip$Drought_flag <-  ifelse(dat.precip$mean == 0, 1, 0)
 
 write.csv(dat.precip, "../Precip_Weather_Daily", row.names = F)
 
+
 dat.precip <- read.csv("../Precip_Weather_Daily")
 
-#Defining the start of a drought for flagging
-#We need to pick how many days before a drought "begins" currently using 9 but this should be discussed
-#dat.start <- dat.precip[dat.precip$days_since_rain == 10,]
-#for(i in 1:nrow(dat.start)){
-#  dat.start[i, "Date"] <- as.Date(dat.start[i, "yday"], origin = paste(dat.start[i, "year"], "-01-01", sep = ""))
-#  dat.start[i, "month"] <- lubridate::month(dat.start[i, "Date"])
-#}
-
-
+#Creating a dataframe that contains the end date of every drought
 #Defining the end of a drought for flagging
 dat.end <- data.frame()
 for(i in 2:nrow(dat.precip)){
