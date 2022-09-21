9710542193988b18bfaba18acdcd87870b63bab5
LucienFitzpatrick <lfitzpatrick@mortonarb.org>
2022-09-21 17:04:16 -0500

    Added harvest labels and updated to new random effect
    We no longer use both GCM and rcp as random effects, but a single object titled "Driver.set" that is the GCM and rcp

--- MortonArb/2_analysis/AGU_Mandifore_analysis.R
+++ MortonArb/2_analysis/AGU_Mandifore_analysis.R
@@ -94,21 +94,32 @@ for(i in 2:nrow(runs.comb)){
   
 }
 
-#Removing any years before/during management
-runs.late <- runs.comb[runs.comb$year >= 2025, ]
+for(i in 1:nrow(runs.comb)){
+  if(runs.comb[i, "year"]<= 2017){
+    runs.comb[i, "harvest"] <- "Pre-harvest"
+  }else if(runs.comb[i, "year"]>= 2018 & runs.comb[i, "year"]<= 2024){
+    runs.comb[i, "harvest"] <- "Harvest"
+  } else {
+    runs.comb[i, "harvest"] <- "Post-harvest"
+  }
+}
 
-write.csv(runs.late, paste0(path.read, "All_runs_yearly.csv"))
+write.csv(runs.comb, paste0(path.read, "All_runs_yearly.csv"), row.names = F)
 
 #--------------------------------------------------------------#
 #Reading in our data if we have it already
 #--------------------------------------------------------------#
 path.read <- "../data/"
 
-runs.late <- read.csv(paste0(path.read, "All_runs_yearly.csv"))
+runs.comb <- read.csv(paste0(path.read, "All_runs_yearly.csv"))
+
+runs.late <- runs.comb[runs.comb$year >= 2025,]
 
 runs.late <- runs.late[!is.na(runs.late$agb.rel.diff),]
 
 runs.late$Management <- factor(runs.late$Management, levels = c("None", "Gap", "Shelter", "Under"))
+
+runs.late$Driver.set <- paste0(runs.late$GCM,"." ,runs.late$rcp)
 
 runs.late$log.agb.rel.diff <- log(runs.late$agb.rel.diff + abs(min(runs.late$agb.rel.diff)) + 1)
 
@@ -121,98 +132,98 @@ runs.late$log.agb.diff <- log(runs.late$agb.diff + abs(min(runs.late$agb.diff)) 
 library(AICcmodavg)
 
 #Just relative precip
-p.test <- lme(agb.rel.diff ~ rel.precip, random=list(GCM=~1, rcp=~1), data = runs.late, method = "ML")
+p.test <- lme(agb.rel.diff ~ rel.precip, random=list(Driver.set=~1), data = runs.late, method = "ML")
 summary(p.test)
 anova(p.test)
 
 #Just VPD
-VPD.test <- lme(agb.rel.diff ~ VPD, random=list(GCM=~1, rcp=~1), data = runs.late, method = "ML")
+VPD.test <- lme(agb.rel.diff ~ VPD, random=list(Driver.set=~1), data = runs.late, method = "ML")
 summary(VPD.test)
 anova(VPD.test)
 
 #Just relative VPD
-rel.VPD.test <- lme(agb.rel.diff ~ rel.VPD, random=list(GCM=~1, rcp=~1), data = runs.late, method = "ML")
+rel.VPD.test <- lme(agb.rel.diff ~ rel.VPD, random=list(Driver.set=~1), data = runs.late, method = "ML")
 summary(rel.VPD.test)
 anova(rel.VPD.test)
 
 #Just relative temp
-tair.test <- lme(agb.rel.diff ~ rel.tair, random=list(GCM=~1, rcp=~1), data = runs.late, method = "ML")
+tair.test <- lme(agb.rel.diff ~ rel.tair, random=list(Driver.set=~1), data = runs.late, method = "ML")
 summary(tair.test)
 anova(tair.test)
 
 #Just Management
-MNG.test <- lme(agb.rel.diff ~ Management, random=list(GCM=~1, rcp=~1), data = runs.late, method = "ML")
+MNG.test <- lme(agb.rel.diff ~ Management, random=list(Driver.set=~1), data = runs.late, method = "ML")
 summary(MNG.test)
 anova(MNG.test)
 
 #Just height.sd
-height.sd.test <- lme(agb.rel.diff ~ height.sd, random=list(GCM=~1, rcp=~1), data = runs.late, method = "ML")
+height.sd.test <- lme(agb.rel.diff ~ height.sd, random=list(Driver.set=~1), data = runs.late, method = "ML")
 summary(height.sd.test)
 anova(height.sd.test)
 
 #relative precip and management
-p.MNG.test <- lme(agb.rel.diff ~ rel.precip*Management, random=list(GCM=~1, rcp=~1), data = runs.late, method = "ML")
+p.MNG.test <- lme(agb.rel.diff ~ rel.precip*Management, random=list(Driver.set=~1), data = runs.late, method = "ML")
 summary(p.MNG.test)
 anova(p.MNG.test)
 
 #VPD and management
-VPD.MNG.test <- lme(agb.rel.diff ~ VPD*Management, random=list(GCM=~1, rcp=~1), data = runs.late, method = "ML")
+VPD.MNG.test <- lme(agb.rel.diff ~ VPD*Management, random=list(Driver.set=~1), data = runs.late, method = "ML")
 summary(VPD.MNG.test)
 anova(VPD.MNG.test)
 
 #Relative vpd and management
-rel.VPD.MNG.test <- lme(agb.rel.diff ~ rel.VPD*Management, random=list(GCM=~1, rcp=~1), data = runs.late, method = "ML")
+rel.VPD.MNG.test <- lme(agb.rel.diff ~ rel.VPD*Management, random=list(Driver.set=~1), data = runs.late, method = "ML")
 summary(rel.VPD.MNG.test)
 anova(rel.VPD.MNG.test)
 
 #Relative temp and management
-tair.MNG.test <- lme(agb.rel.diff ~ rel.tair*Management, random=list(GCM=~1, rcp=~1), data = runs.late, method = "ML")
+tair.MNG.test <- lme(agb.rel.diff ~ rel.tair*Management, random=list(Driver.set=~1), data = runs.late, method = "ML")
 summary(tair.MNG.test)
 anova(tair.MNG.test)
 
 
 #precip and height sd
-p.height.sd.test <- lme(agb.rel.diff ~ rel.precip*height.sd, random=list(GCM=~1, rcp=~1), data = runs.late, method = "ML")
+p.height.sd.test <- lme(agb.rel.diff ~ rel.precip*height.sd, random=list(Driver.set=~1), data = runs.late, method = "ML")
 summary(p.height.sd.test)
 anova(p.height.sd.test)
 
 #VPD and height.sd
-VPD.height.sd.test <- lme(agb.rel.diff ~ VPD*height.sd, random=list(GCM=~1, rcp=~1), data = runs.late, method = "ML")
+VPD.height.sd.test <- lme(agb.rel.diff ~ VPD*height.sd, random=list(Driver.set=~1), data = runs.late, method = "ML")
 summary(VPD.height.sd.test)
 anova(VPD.height.sd.test)
 
 #relative VPD and height sd
-rel.VPD.height.sd.test <- lme(agb.rel.diff ~ rel.VPD*height.sd, random=list(GCM=~1, rcp=~1), data = runs.late, method = "ML")
+rel.VPD.height.sd.test <- lme(agb.rel.diff ~ rel.VPD*height.sd, random=list(Driver.set=~1), data = runs.late, method = "ML")
 summary(rel.VPD.height.sd.test)
 anova(rel.VPD.height.sd.test)
 
 #relative temp and height.sd
-tair.height.sd.test <- lme(agb.rel.diff ~ rel.tair*height.sd, random=list(GCM=~1, rcp=~1), data = runs.late, method = "ML")
+tair.height.sd.test <- lme(agb.rel.diff ~ rel.tair*height.sd, random=list(Driver.set=~1), data = runs.late, method = "ML")
 summary(tair.height.sd.test)
 anova(tair.height.sd.test)
 
 #relative precip, mangement, and height.sd
-p.MNG.height.sd.test <- lme(agb.rel.diff ~ rel.precip*Management*height.sd, random=list(GCM=~1, rcp=~1), data = runs.late, method = "ML")
+p.MNG.height.sd.test <- lme(agb.rel.diff ~ rel.precip*Management*height.sd, random=list(Driver.set=~1), data = runs.late, method = "ML")
 summary(p.MNG.height.sd.test)
 anova(p.MNG.height.sd.test)
 
 #VPD, mangement, and height.sd
-VPD.MNG.height.sd.test <- lme(agb.rel.diff ~ VPD*Management*height.sd, random=list(GCM=~1, rcp=~1), data = runs.late, method = "ML")
+VPD.MNG.height.sd.test <- lme(agb.rel.diff ~ VPD*Management*height.sd, random=list(Driver.set=~1), data = runs.late, method = "ML")
 summary(VPD.MNG.height.sd.test)
 anova(VPD.MNG.height.sd.test)
 
 #relative VPD, mangement, and height.sd
-rel.VPD.MNG.height.sd.test <- lme(agb.rel.diff ~ rel.VPD*Management*height.sd, random=list(GCM=~1, rcp=~1), data = runs.late, method = "ML")
+rel.VPD.MNG.height.sd.test <- lme(agb.rel.diff ~ rel.VPD*Management*height.sd, random=list(Driver.set=~1), data = runs.late, method = "ML")
 summary(rel.VPD.MNG.height.sd.test)
 anova(rel.VPD.MNG.height.sd.test)
 
 #relative tair, mangement, and height.sd
-tair.MNG.height.sd.test <- lme(agb.rel.diff ~ rel.tair*Management*height.sd, random=list(GCM=~1, rcp=~1), data = runs.late, method = "ML")
+tair.MNG.height.sd.test <- lme(agb.rel.diff ~ rel.tair*Management*height.sd, random=list(Driver.set=~1), data = runs.late, method = "ML")
 summary(tair.MNG.height.sd.test)
 anova(tair.MNG.height.sd.test)
 
 #relative precip, relative VPD, and relative temp
-tair.VPD.p.test <- lme(agb.rel.diff ~ rel.precip*rel.VPD*rel.tair, random=list(GCM=~1, rcp=~1), data = runs.late, method = "ML")
+tair.VPD.p.test <- lme(agb.rel.diff ~ rel.precip*rel.VPD*rel.tair, random=list(Driver.set=~1), data = runs.late, method = "ML")
 summary(tair.VPD.p.test)
 anova(tair.VPD.p.test)
 
@@ -220,9 +231,9 @@ models <- list(p.test, VPD.test, rel.VPD.test, tair.test, MNG.test, height.sd.te
                tair.MNG.test, p.height.sd.test, VPD.height.sd.test, rel.VPD.height.sd.test, tair.height.sd.test,
                p.MNG.height.sd.test, VPD.MNG.height.sd.test, rel.VPD.MNG.height.sd.test, tair.MNG.height.sd.test, tair.VPD.p.test)
 
-model.names <- c('Precip', 'VPD', 'Relative.VPD', 'Air.temp', 'Manage', 'Height.sd', 'Precip*Manage', 'VPD*Manage', 'Relative.VPD*Manage',
-                 'Air.temp*Manage', 'Precip*height.sd', 'VPD*height.sd', 'Relative*VPD.height.sd', 'Air.temp*height.sd',
-                 'Precip*Manage*height.sd', 'VPD*Manage*height.sd', 'Relative.VPD*Manage*height.sd', 'Air.temp*Manage*height.sd', 'Air.temp*VPD*Precip')
+model.names <- c('Precip', 'VPD', 'Proportional.VPD', 'Air.temp', 'Manage', 'Height.sd', 'Precip*Manage', 'VPD*Manage', 'Proportional.VPD*Manage',
+                 'Air.temp*Manage', 'Precip*height.sd', 'VPD*height.sd', 'Proportional.VPD*height.sd', 'Air.temp*height.sd',
+                 'Precip*Manage*height.sd', 'VPD*Manage*height.sd', 'Proportional.VPD*Manage*height.sd', 'Air.temp*Manage*height.sd', 'Air.temp*VPD*Precip')
 
 both.diff.aic <- aictab(models, model.names)
 
@@ -240,14 +251,14 @@ both.diff.bic
 #-------------------------------------------------#
 
 #Just precip and management with relative precip
-rel.MNG.agb.both <- lme(agb.rel.diff ~ rel.precip*Management, random=list(GCM=~1, rcp=~1), data = runs.late, method = "ML")
+rel.MNG.agb.both <- lme(agb.rel.diff ~ rel.precip*Management, random=list(Driver.set=~1), data = runs.late, method = "ML")
 summary(rel.MNG.agb.both)
 anova(rel.MNG.agb.both)
 plot(rel.MNG.agb.both)
 
 
 #Just precip and management with relative precip
-rel.MNG.agb.both <- lme(agb.rel.diff ~ VPD*Management, random=list(GCM=~1, rcp=~1), data = runs.late, method = "ML")
+rel.MNG.agb.both <- lme(agb.rel.diff ~ VPD*Management, random=list(Driver.set=~1), data = runs.late, method = "ML")
 summary(rel.MNG.agb.both)
 anova(rel.MNG.agb.both)
 plot(rel.MNG.agb.both)
@@ -255,7 +266,7 @@ plot(rel.MNG.agb.both)
 
 #precip, management, and height sd
 #THIS IS OUR BEST MODEL
-VPD.height.sd.MNG <- lme(agb.rel.diff ~ VPD*Management*height.sd, random=list(GCM=~1, rcp=~1), data = runs.late, method = "ML")
+VPD.height.sd.MNG <- lme(agb.rel.diff ~ VPD*Management*height.sd, random=list(Driver.set=~1), data = runs.late, method = "ML")
 summary(VPD.height.sd.MNG)
 anova(VPD.height.sd.MNG)
 plot(VPD.height.sd.MNG)
@@ -266,25 +277,27 @@ plot(VPD.height.sd.MNG)
 path.figures <- "G:/.shortcut-targets-by-id/0B_Fbr697pd36c1dvYXJ0VjNPVms/MANDIFORE/MANDIFORE_CaseStudy_MortonArb/Drought and heat analysis/Outline Figures"
 
 library(ggplot2)
+cbPalette  <- c("#000000", "#E69F00", "#56B4E9", "#009E73", 
+                       "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
-#Relative agb change vs VPD by management
-png(width= 750, filename= file.path(path.figures, paste0('Relative_Agb_Change_to_VPD_by_Management.png')))
+#Proportional agb change vs VPD by management
+png(width= 750, filename= file.path(path.figures, paste0('Proportional_Agb_Change_to_VPD_by_Management.png')))
 ggplot(data=runs.late)+
-  ggtitle("Relative Change in above ground biomass (AGB) to VPD increases by Management")+
+  ggtitle("Proportional Change in above ground biomass (AGB) to VPD increases by Management")+
   facet_wrap(~Management)+
   geom_point(aes(x=VPD, y = agb.rel.diff, color = rcp))+
-  ylab("Relative change in AGB")+
+  ylab("Proportional change in AGB")+
   theme(plot.title = element_text(size = 16, face = "bold"))
 dev.off()
 
 
-#Relative agb change vs time by management
-png(width= 750, filename= file.path(path.figures, paste0('Relative_Agb_Change_Over_Time_by_Management.png')))
+#Proportional agb change vs time by management
+png(width= 750, filename= file.path(path.figures, paste0('Proportional_Agb_Change_Over_Time_by_Management.png')))
 ggplot(data=runs.late)+
-  ggtitle("Relative Change in above ground biomass (AGB) over time by Management")+
+  ggtitle("Proportional Change in above ground biomass (AGB) over time by Management")+
   facet_wrap(~Management)+
   geom_line(aes(x=year, y = agb.rel.diff, color = GCM, linetype = rcp))+
-  ylab("Relative change in AGB")+
+  ylab("Proportional change in AGB")+
   theme(plot.title = element_text(size = 16, face = "bold"))
 dev.off()
 
@@ -299,14 +312,17 @@ ggplot(data=runs.late)+
   theme(plot.title = element_text(size = 16, face = "bold"))
 dev.off()
 
+runs.late$total.precip <- runs.late$sum
+runs.late$air.temp <- runs.late$tair
+runs.long <- tidyr::gather(runs.late, var, values, air.temp, total.precip, VPD, factor_key=TRUE)
 
 #VPD increasing over time
-png(width= 750, filename= file.path(path.figures, paste0('VPD_Increasing.png')))
-ggplot(data=runs.late)+
-  ggtitle("CMIP5 models show increasing VPD across two different emisisons scenarios")+
-  facet_wrap(~rcp)+
-  geom_point(aes(x = year, y= VPD, color = GCM))+
-  geom_smooth(aes(x = year, y= VPD, color = GCM))+
+png(width= 750, filename= file.path(path.figures, paste0('Weather_Changing.png')))
+ggplot(data=runs.long)+
+  ggtitle("CMIP5 models weather metrics across two different emisisons scenarios")+
+  facet_grid(var~rcp, scales = "free")+
+  geom_point(aes(x = year, y= values, color = GCM))+
+  geom_smooth(aes(x = year, y= values, color = GCM))+
   theme(plot.title = element_text(size = 16, face = "bold"))
 dev.off()
 
@@ -317,18 +333,22 @@ ggplot(data=runs.late)+
   ggtitle("Above ground biomass (AGB) vs. standard deviaiton of tree height by management")+
   facet_wrap(~Management)+
   geom_point(aes(x=height.sd, y = agb, color = rcp))+
+  geom_point(data = runs.comb[runs.comb$harvest == "Pre-harvest" | runs.comb$harvest == "Harvest",], aes(x=height.sd, y=agb, color = harvest))+
+  scale_colour_manual(values=cbPalette)+
   ylab("AGB")+
   xlab("SD of tree height")+
   theme(plot.title = element_text(size = 16, face = "bold"))
 dev.off()
 
-#Relative AGB over height sd by management
-png(width= 800, filename= file.path(path.figures, paste0('Relative_change_in_AGB_vs_Height_sd_by_Management.png')))
+#Proportional AGB over height sd by management
+png(width= 800, filename= file.path(path.figures, paste0('Proportional_change_in_AGB_vs_Height_sd_by_Management.png')))
 ggplot(data=runs.late)+
-  ggtitle("Relative change in above ground biomass (AGB) vs. standard deviaiton of tree height by management")+
+  ggtitle("Proportional change in above ground biomass (AGB) vs. standard deviaiton of tree height by management")+
   facet_wrap(~Management)+
-  geom_point(aes(x=VPD, y = height.sd, color = rcp))+
-  ylab("Relative change in AGB")+
+  geom_point(aes(x=height.sd, y = agb.rel.diff, color = rcp))+
+  geom_point(data = runs.comb[runs.comb$harvest == "Pre-harvest" | runs.comb$harvest == "Harvest",], aes(x=height.sd, y=agb.rel.diff, color = harvest))+
+  scale_colour_manual(values=cbPalette)+
+  ylab("Proportional change in AGB")+
   xlab("SD of tree height")+
   theme(plot.title = element_text(size = 14, face = "bold"))
 dev.off()
@@ -358,22 +378,23 @@ dev.off()
 
 
 #Boxplot I'm still figuring out
-png(width= 750, filename= file.path(path.figures, paste0('Height_sd_by_Management.png')))
+png(width= 750, filename= file.path(path.figures, paste0('Dist_of_Height_sd_by_Management.png')))
 ggplot(data=runs.late)+
-  ggtitle("Standard Deviation of Height by Management")+
-  #facet_wrap(~Management)+
-  geom_boxplot(aes(x=Management, y =height.sd))+
-  ylab("Standard deviation of height")+
+  ggtitle("Distribution of Standard Deviation of Height by Management")+
+  facet_wrap(~Management)+
+  geom_histogram(aes(x =height.sd))+
+  ylab("Frequency")+
   theme(plot.title = element_text(size = 16, face = "bold"))
 dev.off()
 
 
-png(width= 750, filename= file.path(path.figures, paste0('Relative_change_in_Agb_by_Management.png')))
+png(width= 750, filename= file.path(path.figures, paste0('Proportional_change_in_Agb_by_Management.png')))
 ggplot(data=runs.late)+
-  ggtitle("Relative change in agb by Management")+
-  #facet_wrap(~Management)+
-  geom_boxplot(aes(x=Management, y =agb.rel.diff))+
-  ylab("Relative change in agb")+
+  ggtitle("Proportional change in agb by Management")+
+  facet_wrap(~Management)+
+  geom_histogram(aes(x =agb.rel.diff))+
+  xlab("Proportional change in agb")+
+  ylab("Frequency")+
   theme(plot.title = element_text(size = 16, face = "bold"))
 dev.off()
 
@@ -388,7 +409,7 @@ runs.lag <- runs.late[!is.na(runs.late$agb.rel.lag1),]
 runs.lag$Management <- factor(runs.lag$Management, levels = c("None", "Gap", "Shelter", "Under"))
 
 #Lag of one year
-p.MNG.agb.lag1 <- lme(agb.rel.lag1 ~ rel.VPD*Management*height.sd, random=list(GCM=~1, rcp=~1), data = runs.lag, method = "ML")
+p.MNG.agb.lag1 <- lme(agb.rel.lag1 ~ rel.VPD*Management*height.sd, random=list(Driver.set=~1), data = runs.lag, method = "ML")
 summary(p.MNG.agb.lag1)
 anova(p.MNG.agb.lag1)
 plot(p.MNG.agb.lag1)
