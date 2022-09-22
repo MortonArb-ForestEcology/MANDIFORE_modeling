ae3dc08aeaf4aa7bf46f7d5b58b591fdd9dcdd8b
LucienFitzpatrick <lfitzpatrick@mortonarb.org>
2022-09-22 15:18:29 -0500

    Added boxplots of change overtime

--- MortonArb/2_analysis/AGU_Mandifore_analysis.R
+++ MortonArb/2_analysis/AGU_Mandifore_analysis.R
@@ -330,13 +330,12 @@ ggplot(data=runs.long)+
   theme(plot.title = element_text(size = 16, face = "bold"))
 dev.off()
 
-
 #AGB over height sd by management
 png(width= 750, filename= file.path(path.figures, paste0('AGB_vs_Height_sd_by_Management.png')))
 ggplot(data=runs.late)+
   ggtitle("Above ground biomass (AGB) vs. standard deviaiton of tree height by management")+
   facet_wrap(~Management)+
-  geom_point(aes(x=height.sd, y = agb, color = rcp))+
+  geom_point(aes(x=height.sd, y = agb, color = harvest, shape = rcp))+
   geom_point(data = runs.comb[runs.comb$harvest == "Pre-harvest" | runs.comb$harvest == "Harvest",], aes(x=height.sd, y=agb, color = harvest))+
   scale_colour_manual(values=cbPalette)+
   ylab("AGB")+
@@ -344,18 +343,35 @@ ggplot(data=runs.late)+
   theme(plot.title = element_text(size = 16, face = "bold"))
 dev.off()
 
+
 #Proportional AGB over height sd by management
 png(width= 800, filename= file.path(path.figures, paste0('Proportional_change_in_AGB_vs_Height_sd_by_Management.png')))
 ggplot(data=runs.late)+
   ggtitle("Proportional change in above ground biomass (AGB) vs. standard deviaiton of tree height by management")+
   facet_wrap(~Management)+
-  geom_point(aes(x=height.sd, y = agb.rel.diff, color = rcp))+
+  geom_point(aes(x=height.sd, y = agb.rel.diff, color = harvest, shape = rcp))+
   geom_point(data = runs.comb[runs.comb$harvest == "Pre-harvest" | runs.comb$harvest == "Harvest",], aes(x=height.sd, y=agb.rel.diff, color = harvest))+
   scale_colour_manual(values=cbPalette)+
   ylab("Proportional change in AGB")+
   xlab("SD of tree height")+
   theme(plot.title = element_text(size = 14, face = "bold"))
 dev.off()
+
+
+#Proportional AGB over AGB by management
+png(width= 800, filename= file.path(path.figures, paste0('Proportional_change_in_AGB_vs_AGB_by_Management.png')))
+ggplot(data=runs.late)+
+  ggtitle("Proportional change in above ground biomass (AGB) vs. AGB by management")+
+  facet_wrap(~Management)+
+  geom_point(aes(x=agb, y = agb.rel.diff, color = harvest, shape = rcp))+
+  geom_point(data = runs.comb[runs.comb$harvest == "Pre-harvest" | runs.comb$harvest == "Harvest",], aes(x=agb, y=agb.rel.diff, color = harvest))+
+  scale_colour_manual(values=cbPalette)+
+  ylab("Proportional change in AGB")+
+  xlab("SD of tree height")+
+  theme(plot.title = element_text(size = 14, face = "bold"))
+dev.off()
+
+
 
 #height sd vs VPD by Management
 png(width= 750, filename= file.path(path.figures, paste0('Height_sd_vs_VPD_by_Management.png')))
@@ -374,14 +390,14 @@ png(width= 750, filename= file.path(path.figures, paste0('Height_sd_vs_time_by_M
 ggplot(data=runs.late)+
   ggtitle("Standard Deviation of Height over time by Management")+
   facet_wrap(~Management)+
-  geom_line(aes(x=year, y = height.sd, color= GCM, linetype = rcp))+
+  geom_line(aes(x=year, y = height.sd, group= Driver.set, color = rcp))+
   ylab("SD of tree height")+
   xlab("year")+
   theme(plot.title = element_text(size = 16, face = "bold"))
 dev.off()
 
 
-#Boxplot I'm still figuring out
+#Distribution of height.sd
 png(width= 750, filename= file.path(path.figures, paste0('Dist_of_Height_sd_by_Management.png')))
 ggplot(data=runs.late)+
   ggtitle("Distribution of Standard Deviation of Height by Management")+
@@ -391,7 +407,7 @@ ggplot(data=runs.late)+
   theme(plot.title = element_text(size = 16, face = "bold"))
 dev.off()
 
-
+#Distribution of proportional change in agb
 png(width= 750, filename= file.path(path.figures, paste0('Proportional_change_in_Agb_by_Management.png')))
 ggplot(data=runs.late)+
   ggtitle("Proportional change in agb by Management")+
@@ -400,6 +416,46 @@ ggplot(data=runs.late)+
   xlab("Proportional change in agb")+
   ylab("Frequency")+
   theme(plot.title = element_text(size = 16, face = "bold"))
+dev.off()
+
+#Boxplot section
+runs.comb$Driver.set <- paste0(runs.comb$GCM,"." ,runs.comb$rcp)
+
+box.df <- data.frame()
+for(DRIVE in unique(runs.comb$Driver.set)){
+  for(MNG in unique(runs.comb$Management)){
+  temp <- runs.comb[runs.comb$Driver.set == DRIVE & runs.comb$Management == MNG, ]
+  temp$height.sd.diff <- mean(temp[temp$year >= 2089, "height.sd"]) - mean(temp[temp$year <= 2017, "height.sd"])
+  temp$dbh.sd.diff <-  mean(temp[temp$year >= 2089, "dbh.sd"]) - mean(temp[temp$year <= 2017, "dbh.sd"])
+  temp$agb.change <-  mean(temp[temp$year >= 2089, "agb"]) - mean(temp[temp$year <= 2017, "agb"])
+  out.df <- data.frame(unique(temp$Driver.set),unique(temp$Management), unique(temp$height.sd.diff), unique(temp$dbh.sd.diff), unique(temp$agb.change))
+  colnames(out.df) <- c("Driver.set","Management" ,"height.sd.diff", "dbh.sd.diff", "agb.change")
+  box.df <- rbind(box.df, out.df)
+  }
+}
+
+png(width= 750, filename= file.path(path.figures, paste0('Boxplot_of_height_sd_change.png')))
+ggplot(data = box.df)+
+  ggtitle("Change in SD of tree height from first 10 years to last 10 years")+
+  geom_boxplot(aes(x=Management, y=height.sd.diff))+
+  ylab("SD of tree height")+
+  xlab("Management")
+dev.off()
+
+png(width= 750, filename= file.path(path.figures, paste0('Boxplot_of_dbh_sd_change.png')))
+ggplot(data = box.df)+
+  ggtitle("Change in SD of DBH from first 10 years to last 10 years")+
+  geom_boxplot(aes(x=Management, y=dbh.sd.diff))+
+  ylab("SD of DBH")+
+  xlab("Management")
+dev.off()
+
+png(width= 750, filename= file.path(path.figures, paste0('Boxplot_of_AGB_change.png')))
+ggplot(data = box.df)+
+  ggtitle("Change in AGB from first 10 years to last 10 years")+
+  geom_boxplot(aes(x=Management, y=agb.change))+
+  ylab("Aboveground biomass")+
+  xlab("Management")
 dev.off()
 
 #------------------------------------------------#
