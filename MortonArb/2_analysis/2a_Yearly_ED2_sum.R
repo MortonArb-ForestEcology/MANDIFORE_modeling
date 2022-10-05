d45e426883e5ad5e78f4af45f9d6494ef8e31281
Christy Rollinson <crollinson@gmail.com>
2022-10-05 13:17:49 -0500

    streamline processing of annual values

--- MortonArb/2_analysis/2a_Yearly_ED2_sum.R
+++ MortonArb/2_analysis/2a_Yearly_ED2_sum.R
@@ -19,7 +19,8 @@ path.google <- "/Volumes/GoogleDrive/My Drive/MANDIFORE/MANDIFORE_CaseStudy_Mort
 #----------------------------------------------------------------#
 #Reading in weather data
 #----------------------------------------------------------------#
-dat.precip <- read.csv(file.path(path.google, "processed_data/Precip_Weather_Daily.csv"))
+# dat.precip <- read.csv(file.path(path.google, "processed_data/Precip_Weather_Daily.csv"))
+dat.precip <- read.csv(file.path(path.google, "processed_data/Met_Precip_Daily.csv"))
 
 dat.precip$year <- lubridate::year(dat.precip$Date)
 
@@ -28,20 +29,23 @@ dat.precip <- dat.precip[!is.na(dat.precip$mean),]
 #Tracking days without rain
 dat.precip$no.rain <- ifelse(dat.precip$sum == 0, 1 ,0)
 
-dat.year <- aggregate(sum~year+model+scenario, dat.precip, FUN = sum)
+dat.year <- aggregate(precip.mean~year+model+scenario, dat.precip, FUN = mean)
+dat.year$precip.total <- dat.year$precip.mean*365*24*60*60 # should now be in mm/yr
+
+# dat.year <- aggregate(sum~year+model+scenario, dat.precip, FUN = sum)
 
 dat.year$rainless.days <- aggregate(no.rain~year+model+scenario, dat.precip, FUN = sum)[, "no.rain"]
 
 #--------------------------------------------------------------#
 #Reading in the Mandifore data
 #--------------------------------------------------------------#
-runs.all <- read_bulk(directory = file.path(path.google, "processed_data"), extension = "Site.csv", header = TRUE)
+runs.all <- read_bulk(directory = file.path(path.google, "output"), extension = "Site.csv", header = TRUE)
 summary(runs.all)
 
 runs.all$Management <- car::recode(runs.all$Management, "'MgmtNone'='None'; 'MgmtGap'='Gap'; 'MgmtShelter'='Shelter'; 'MgmtUnder'='Under'")
 
 #REMOVING MIROC_ESM_CHEM BECUSE WE DONT HAVE RCP 45 data
-runs.all <- runs.all[runs.all$GCM != "MIROC-ESM-CHEM",]
+runs.all <- runs.all[runs.all$GCM != "MIROC-ESM-CHEM" & runs.all$year>2006,]
 
 #Aggregating values by mean
 runs.year <- aggregate(cbind(tair, VPD, agb, lai, npp,soil.moist.deep, soil.moist.surf ,
@@ -51,67 +55,42 @@ runs.year <- aggregate(cbind(tair, VPD, agb, lai, npp,soil.moist.deep, soil.mois
 runs.comb <- merge(runs.year, dat.year, by.x = c("year", "GCM", "rcp"), by.y = c("year", "model", "scenario"))
 
 runs.comb$Management <- factor(runs.comb$Management, levels = c("None", "Gap", "Shelter", "Under"))
+runs.comb$harvest <- ifelse(runs.comb$year<2020, "pre-harvest", ifelse(runs.comb$year %in% 2020:2024, "harvest", "post-harvest")) 
 
 #Calculating our agb metrics for evaluation
-for(i in 2:nrow(runs.comb)){
-  
-  #Difference between current year and previous year
-  GCM <- runs.comb[i, "GCM"]
-  rcp <- runs.comb[i, "rcp"]
-  MNG <- runs.comb[i, "Management"]
-  Year <- runs.comb[i, "year"]
-  
-  #Calculating the average weather for the first 12 years of model run
-  mean.precip <- mean(runs.comb[runs.comb$GCM==GCM & runs.comb$rcp == rcp & runs.comb$Management==MNG & runs.comb$year <2020, "sum"])
-  mean.VPD <- mean(runs.comb[runs.comb$GCM==GCM & runs.comb$rcp == rcp & runs.comb$Management==MNG & runs.comb$year <2020, "VPD"])
-  mean.tair <- mean(runs.comb[runs.comb$GCM==GCM & runs.comb$rcp == rcp & runs.comb$Management==MNG & runs.comb$year <2020, "tair"])
-  
-  #Calculating the relative difference in a metric from the mean
-  runs.comb[i, "rel.precip"] <- (runs.comb[i, "sum"] - mean.precip)/mean.precip
-  runs.comb[i, "rel.VPD"] <- (runs.comb[i, "VPD"] - mean.VPD)/mean.VPD
-  runs.comb[i, "rel.tair"] <- (runs.comb[i, "tair"] - mean.tair)/mean.tair
-  
-  if(Year > 2006 & Year < 2098){
-    
-    #relative change in precip from one year to the next
-    runs.comb[i, "diff.precip"] <- (runs.comb[i, "sum"]- runs.comb[runs.comb$GCM==GCM & runs.comb$rcp == rcp & runs.comb$Management==MNG & runs.comb$year == Year-1, "sum"])/runs.comb[runs.comb$GCM==GCM & runs.comb$rcp == rcp & runs.comb$Management==MNG & runs.comb$year == Year-1, "sum"]
-    
-    #Change in agb
-    runs.comb[i, "agb.diff"] <- runs.comb[i, "agb"] - runs.comb[runs.comb$GCM==GCM & runs.comb$rcp == rcp & runs.comb$Management==MNG & runs.comb$year == Year-1, "agb"]  
-    
-    #Relative change in agb
-    runs.comb[i, "agb.rel.diff"] <- (runs.comb[i, "agb"]- runs.comb[runs.comb$GCM==GCM & runs.comb$rcp == rcp & runs.comb$Management==MNG & runs.comb$year == Year-1, "agb"])/runs.comb[runs.comb$GCM==GCM & runs.comb$rcp == rcp & runs.comb$Management==MNG & runs.comb$year == Year-1, "agb"] 
-    
-    #Relative future change in agb by 2 years
-    
-    runs.comb[i, "agb.rel.lead2"] <- (runs.comb[runs.comb$GCM==GCM & runs.comb$rcp == rcp & runs.comb$Management==MNG & runs.comb$year == Year+2, "agb"] - runs.comb[i, "agb"])/runs.comb[i, "agb"]  
-    
-  }else if(Year>2097){ #This section exists for the last two years where we can't calculate the lead because the model run ends
-    #relative change in precip from one year to the next
-    runs.comb[i, "diff.precip"] <- (runs.comb[i, "sum"]- runs.comb[runs.comb$GCM==GCM & runs.comb$rcp == rcp & runs.comb$Management==MNG & runs.comb$year == Year-1, "sum"])/runs.comb[runs.comb$GCM==GCM & runs.comb$rcp == rcp & runs.comb$Management==MNG & runs.comb$year == Year-1, "sum"]
-    
-    #Change in agb
-    runs.comb[i, "agb.diff"] <- runs.comb[i, "agb"] - runs.comb[runs.comb$GCM==GCM & runs.comb$rcp == rcp & runs.comb$Management==MNG & runs.comb$year == Year-1, "agb"]  
-    
-    #Relative change in agb
-    runs.comb[i, "agb.rel.diff"] <- (runs.comb[i, "agb"]- runs.comb[runs.comb$GCM==GCM & runs.comb$rcp == rcp & runs.comb$Management==MNG & runs.comb$year == Year-1, "agb"])/runs.comb[runs.comb$GCM==GCM & runs.comb$rcp == rcp & runs.comb$Management==MNG & runs.comb$year == Year-1, "agb"] 
-    
-    #Relative future change in agb by 2 years
-    runs.comb[i, "agb.rel.lead2"] <- NA  
-    
+runs.comb[,c("agb.diff", "agb.rel.diff")] <- NA
+for(GCM in unique(runs.comb$GCM)){
+  for(RCP in unique(runs.comb$rcp[runs.comb$GCM==GCM])){
+    for(MGMT in unique(runs.comb$Management[runs.comb$GCM==GCM & runs.comb$rcp==RCP])){
+      run.rows <- which(runs.comb$Management==MGMT & runs.comb$rcp==RCP & runs.comb$GCM==GCM)
+      rows.ref <- which(runs.comb$Management==MGMT & runs.comb$rcp==RCP & runs.comb$GCM==GCM & runs.comb$year<2020)
+      
+      #Calculating the average weather for the first 12 years of model run
+      mean.precip <- mean(runs.comb[rows.ref, "precip.total"])
+      mean.VPD <- mean(runs.comb[rows.ref, "VPD"])
+      mean.tair <- mean(runs.comb[rows.ref, "tair"])
+      
+      #Calculating the relative difference in a metric from the mean
+      # This percent difference only works when the 0 has a real value; doesn't work for temp!
+      runs.comb[run.rows, "diff.precip"] <- runs.comb[run.rows, "precip.total"] - mean.precip
+      runs.comb[run.rows, "rel.precip"] <- (runs.comb[run.rows, "precip.total"] - mean.precip)/mean.precip
+      runs.comb[run.rows, "diff.VPD"] <- runs.comb[run.rows, "VPD"] - mean.VPD
+      runs.comb[run.rows, "rel.VPD"] <- (runs.comb[run.rows, "VPD"] - mean.VPD)/mean.VPD
+      runs.comb[run.rows, "diff.tair"] <- runs.comb[run.rows, "tair"] - mean.tair
+      
+      # Calculating running change in AGB
+      dat.run <- runs.comb[run.rows,]
+      for(YR in 2008:2099){
+        agb.now <- dat.run$agb[dat.run$year==YR]
+        agb.past <- dat.run$agb[dat.run$year==(YR-1)]
+        dat.run$agb.diff[dat.run$year==YR] <- agb.now - agb.past
+        dat.run$agb.rel.diff[dat.run$year==YR] <- (agb.now - agb.past)/agb.past
+      }
+      runs.comb[run.rows, c("agb.diff", "agb.rel.diff")] <- dat.run[,c("agb.diff", "agb.rel.diff")]
+      
+    }
   }
-  
 }
 
-#Adding harvest flag markers
-for(i in 1:nrow(runs.comb)){
-  if(runs.comb[i, "year"]< 2020){
-    runs.comb[i, "harvest"] <- "Pre-harvest"
-  }else if(runs.comb[i, "year"]>= 2020 & runs.comb[i, "year"]<= 2024){
-    runs.comb[i, "harvest"] <- "Harvest"
-  } else {
-    runs.comb[i, "harvest"] <- "Post-harvest"
-  }
-}
 
 write.csv(runs.comb, paste0("../data/All_runs_yearly.csv"), row.names = F)
