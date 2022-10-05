138733ccb595dee75e1e691305a0855cdede1c81
LucienFitzpatrick <lfitzpatrick@mortonarb.org>
2022-10-05 09:29:14 -0500

    Updated file paths

--- MortonArb/2_analysis/2a_Yearly_ED2_sum.R
+++ MortonArb/2_analysis/2a_Yearly_ED2_sum.R
@@ -12,13 +12,13 @@ library(lubridate)
 library(nlme)
 library(AICcmodavg)
 
-path.google <- "/Volumes/GoogleDrive/My Drive/MANDIFORE/MANDIFORE_CaseStudy_MortonArb/output/"
+path.google <- "/Volumes/GoogleDrive/My Drive/MANDIFORE/MANDIFORE_CaseStudy_MortonArb/"
 
 
 #----------------------------------------------------------------#
 #Reading in weather data
 #----------------------------------------------------------------#
-dat.precip <- read.csv(file.path(path.google, "Precip_Weather_Daily.csv"))
+dat.precip <- read.csv(file.path(path.google, "processed_data/Precip_Weather_Daily.csv"))
 
 dat.precip$year <- lubridate::year(dat.precip$Date)
 
@@ -34,7 +34,7 @@ dat.year$rainless.days <- aggregate(no.rain~year+model+scenario, dat.precip, FUN
 #--------------------------------------------------------------#
 #Reading in the Mandifore data
 #--------------------------------------------------------------#
-runs.all <- read.csv(paste0(path.google, "All_runs.csv"))
+runs.all <- read.csv(paste0(path.google, "processed_data/All_runs.csv"))
 
 runs.all$Management <- car::recode(runs.all$Management, "'MgmtNone'='None'; 'MgmtGap'='Gap'; 'MgmtShelter'='Shelter'; 'MgmtUnder'='Under'")
 
