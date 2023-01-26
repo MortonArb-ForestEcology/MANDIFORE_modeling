1b7f30d4d0c2fada3d01a484ee47b61ec7e12c08
LucienFitzpatrick <lfitzpatrick@mortonarb.org>
2023-01-26 13:21:17 -0600

    Added generic copies for relative weather and structure
    They will need to be changed and updated but I made copies of my original analysis so we now have a version that looks at relative weather and structural variables. They still suffer from the issue of not reporting effects for lag -5 and None management. I'm really struggling to fix that and might need to leave that on the burner for when Christy gets back.

--- MortonArb/2_analysis/6a_SEA_analysis.R
+++ MortonArb/2_analysis/6a_SEA_analysis.R
@@ -83,32 +83,27 @@ summary(runs.yr)
 # --------------
 # Running the calculation
 # --------------
-# Setting up a table to stick our output
-struc.var <- c("agb", "density.tree", "tree.dbh.mean", "tree.dbh.sd")
-met.var <- c("tair", "VPD", "precip.total", "rel.VPD", "rel.precip", "diff.tair")
-
+#Working with non-relative weather metrics
+met.var <- c("tair", "VPD", "precip.total")
 df.lag.metxmng <- data.frame()
-  for(COL in met.var){
+for(COL in met.var){
     
-      mod.lag <- nlme::lme(eval(substitute(j ~ as.factor(one.crash)*as.factor(Management)-1, list(j = as.name(COL)))), random=list(rcp = ~1, GCM =~1), data = runs.yr[!is.na(runs.yr$one.crash) & runs.yr$rcp == RCP,], na.action = na.omit)
+    mod.lag <- nlme::lme(eval(substitute(j ~ as.factor(one.crash)*as.factor(Management)-1, list(j = as.name(COL)))), random=list(rcp = ~1, GCM =~1), data = runs.yr[!is.na(runs.yr$one.crash) & runs.yr$rcp == RCP,], na.action = na.omit)
       
-      output <- summary(mod.lag)
-      # mod.sum$tTable
+    output <- summary(mod.lag)
       
-      lag.list.metxmng <- list()
-      lag.list.metxmng[[paste(COL)]]$VAR <- COL
-      lag.list.metxmng[[paste(COL)]]$Comp <- rownames(output$tTable)
-      lag.list.metxmng[[paste(COL)]]$estimate <- output$tTable[,"Value"]
-      lag.list.metxmng[[paste(COL)]]$std.err <- output$tTable[,"Std.Error"]
-      lag.list.metxmng[[paste(COL)]]$t.stat <- output$tTable[,"t-value"]
-      lag.list.metxmng[[paste(COL)]]$p.val <- output$tTable[,"p-value"]
-      temp.lag.metxmng <- dplyr::bind_rows(lag.list.metxmng)
-      temp.lag.metxmng$lag <- c(-5,-4,-3,-2,-1,0, NA, NA, NA, rep(unique(-4:0), times = 3))
-      temp.lag.metxmng$MNG <- c(NA, NA, NA, NA, NA, NA, "Under", "Shelter", "Gap", rep(c("Under", "Shelter", "Gap"), each = 5))
+    lag.list.metxmng <- list()
+    lag.list.metxmng[[paste(COL)]]$VAR <- COL
+    lag.list.metxmng[[paste(COL)]]$Comp <- rownames(output$tTable)
+    lag.list.metxmng[[paste(COL)]]$estimate <- output$tTable[,"Value"]
+    lag.list.metxmng[[paste(COL)]]$std.err <- output$tTable[,"Std.Error"]
+    lag.list.metxmng[[paste(COL)]]$t.stat <- output$tTable[,"t-value"]
+    lag.list.metxmng[[paste(COL)]]$p.val <- output$tTable[,"p-value"]
+    temp.lag.metxmng <- dplyr::bind_rows(lag.list.metxmng)
+    temp.lag.metxmng$lag <- c(-5,-4,-3,-2,-1,0, NA, NA, NA, rep(unique(-4:0), times = 3))
+    temp.lag.metxmng$MNG <- c(NA, NA, NA, NA, NA, NA, "Under", "Shelter", "Gap", rep(c("Under", "Shelter", "Gap"), each = 5))
       
-      df.lag.metxmng <- rbind(df.lag.metxmng, temp.lag.metxmng)
-      
-      
+    df.lag.metxmng <- rbind(df.lag.metxmng, temp.lag.metxmng)
   }
 summary(df.lag.metxmng)
 
@@ -123,4 +118,76 @@ ggplot(data=df.lag.metxmng ) +
         panel.grid = element_blank(),
         panel.background=element_rect(fill=NA, color="black"))
 
+
+#Looking at relative weather
+relmet.var <- c("rel.precip", "diff.tair", "rel.VPD")
+df.lag.relxmng <- data.frame()
+for(COL in relmet.var){
+  
+  mod.lag <- nlme::lme(eval(substitute(j ~ as.factor(one.crash)*as.factor(Management)-1, list(j = as.name(COL)))), random=list(rcp = ~1, GCM =~1), data = runs.yr[!is.na(runs.yr$one.crash) & runs.yr$rcp == RCP,], na.action = na.omit)
+  
+  output <- summary(mod.lag)
+  
+  lag.list.relxmng <- list()
+  lag.list.relxmng[[paste(COL)]]$VAR <- COL
+  lag.list.relxmng[[paste(COL)]]$Comp <- rownames(output$tTable)
+  lag.list.relxmng[[paste(COL)]]$estimate <- output$tTable[,"Value"]
+  lag.list.relxmng[[paste(COL)]]$std.err <- output$tTable[,"Std.Error"]
+  lag.list.relxmng[[paste(COL)]]$t.stat <- output$tTable[,"t-value"]
+  lag.list.relxmng[[paste(COL)]]$p.val <- output$tTable[,"p-value"]
+  temp.lag.relxmng <- dplyr::bind_rows(lag.list.relxmng)
+  temp.lag.relxmng$lag <- c(-5,-4,-3,-2,-1,0, NA, NA, NA, rep(unique(-4:0), times = 3))
+  temp.lag.relxmng$MNG <- c(NA, NA, NA, NA, NA, NA, "Under", "Shelter", "Gap", rep(c("Under", "Shelter", "Gap"), each = 5))
+  
+  df.lag.relxmng <- rbind(df.lag.relxmng, temp.lag.relxmng)
+}
+summary(df.lag.relxmng)
+
+ggplot(data=df.lag.relxmng ) +
+  facet_wrap(MNG~VAR, scales = "free_y") +
+  geom_bar(data=df.lag.relxmng[!is.na(df.lag.relxmng$p.val) & df.lag.relxmng$p.val>=0.001,], aes(x=as.factor(lag), y=estimate), stat="identity", fill="gray50") +
+  # geom_vline(xintercept=as.factor(0), color="red") +
+  geom_bar(data=df.lag.relxmng[!is.na(df.lag.relxmng$p.val) & df.lag.relxmng$p.val<0.001,], aes(x=as.factor(lag), y=estimate), stat="identity", fill="black") +
+  geom_bar(data=df.lag.relxmng[!is.na(df.lag.relxmng$p.val) & df.lag.relxmng$p.val<0.001 & df.lag.relxmng$lag==0,], aes(x=as.factor(lag), y=estimate), stat="identity", fill="red") +
+  geom_bar(data=df.lag.relxmng[!is.na(df.lag.relxmng$p.val) & df.lag.relxmng$p.val>=0.001 & df.lag.relxmng$lag==0,], aes(x=as.factor(lag), y=estimate), stat="identity", fill="red", alpha=0.5) +
+  theme(panel.spacing = unit(0, "lines"),
+        panel.grid = element_blank(),
+        panel.background=element_rect(fill=NA, color="black"))
+
+
+
+#Looking at structural metrics
+struc.var <- c("agb", "density.tree", "tree.dbh.mean", "tree.dbh.sd")
+df.lag.strucxmng <- data.frame()
+for(COL in struc.var){
+  
+  mod.lag <- nlme::lme(eval(substitute(j ~ as.factor(one.crash)*as.factor(Management)-1, list(j = as.name(COL)))), random=list(rcp = ~1, GCM =~1), data = runs.yr[!is.na(runs.yr$one.crash) & runs.yr$rcp == RCP,], na.action = na.omit)
+  
+  output <- summary(mod.lag)
+  
+  lag.list.strucxmng <- list()
+  lag.list.strucxmng[[paste(COL)]]$VAR <- COL
+  lag.list.strucxmng[[paste(COL)]]$Comp <- rownames(output$tTable)
+  lag.list.strucxmng[[paste(COL)]]$estimate <- output$tTable[,"Value"]
+  lag.list.strucxmng[[paste(COL)]]$std.err <- output$tTable[,"Std.Error"]
+  lag.list.strucxmng[[paste(COL)]]$t.stat <- output$tTable[,"t-value"]
+  lag.list.strucxmng[[paste(COL)]]$p.val <- output$tTable[,"p-value"]
+  temp.lag.strucxmng <- dplyr::bind_rows(lag.list.strucxmng)
+  temp.lag.strucxmng$lag <- c(-5,-4,-3,-2,-1,0, NA, NA, NA, rep(unique(-4:0), times = 3))
+  temp.lag.strucxmng$MNG <- c(NA, NA, NA, NA, NA, NA, "Under", "Shelter", "Gap", rep(c("Under", "Shelter", "Gap"), each = 5))
+  
+  df.lag.strucxmng <- rbind(df.lag.strucxmng, temp.lag.strucxmng)
+}
+summary(df.lag.strucxmng)
+
+ggplot(data=df.lag.strucxmng ) +
+  facet_wrap(MNG~VAR, scales = "free_y") +
+  geom_bar(data=df.lag.strucxmng[!is.na(df.lag.strucxmng$p.val) & df.lag.strucxmng$p.val>=0.001,], aes(x=as.factor(lag), y=estimate), stat="identity", fill="gray50") +
+  # geom_vline(xintercept=as.factor(0), color="red") +
+  geom_bar(data=df.lag.strucxmng[!is.na(df.lag.strucxmng$p.val) & df.lag.strucxmng$p.val<0.001,], aes(x=as.factor(lag), y=estimate), stat="identity", fill="black") +
+  geom_bar(data=df.lag.strucxmng[!is.na(df.lag.strucxmng$p.val) & df.lag.strucxmng$p.val<0.001 & df.lag.strucxmng$lag==0,], aes(x=as.factor(lag), y=estimate), stat="identity", fill="red") +
+  geom_bar(data=df.lag.strucxmng[!is.na(df.lag.strucxmng$p.val) & df.lag.strucxmng$p.val>=0.001 & df.lag.strucxmng$lag==0,], aes(x=as.factor(lag), y=estimate), stat="identity", fill="red", alpha=0.5) +
+  theme(panel.spacing = unit(0, "lines"),
+        panel.grid = element_blank(),
+        panel.background=element_rect(fill=NA, color="black"))
 
