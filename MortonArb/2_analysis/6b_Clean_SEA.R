e51767bb375a404e41ee4e0b150d3a8e0035382e
LucienFitzpatrick <lfitzpatrick@mortonarb.org>
2023-06-22 15:24:55 -0500

    QAQC changes to match other figures
    Changed gap to group, crash to loss, and gave better figure axis.

--- MortonArb/2_analysis/6b_Clean_SEA.R
+++ MortonArb/2_analysis/6b_Clean_SEA.R
@@ -11,14 +11,15 @@ library(nlme)
 library(multcomp)
 library(dplyr)
 #------------------------------------------------------------------------#
-path.google <- "~/Google Drive/My Drive/MANDIFORE/MANDIFORE_CaseStudy_MortonArb/"
+#path.google <- "~/Google Drive/My Drive/MANDIFORE/MANDIFORE_CaseStudy_MortonArb/"
 
-# path.google <- "G:/.shortcut-targets-by-id/0B_Fbr697pd36c1dvYXJ0VjNPVms/MANDIFORE/MANDIFORE_CaseStudy_MortonArb/"
+path.google <- "G:/.shortcut-targets-by-id/0B_Fbr697pd36c1dvYXJ0VjNPVms/MANDIFORE/MANDIFORE_CaseStudy_MortonArb/"
 
 path.figures <- file.path(path.google, "Drought and heat analysis/Figures/SEA figures/")
 
 runs.yr <- read.csv(file.path(path.google, "processed_data/All_runs_yearly.csv"))
-runs.yr$Management <- factor(runs.yr$Management, levels=c("None", "Under", "Shelter", "Gap"))
+runs.yr$Management <- car::recode(runs.yr$Management, "'None'='None'; 'Gap'='Group'; 'Shelter'='Shelter'; 'Under'='Under'")
+runs.yr$Management <- factor(runs.yr$Management, levels = c("None", "Under", "Shelter", "Group"))
 runs.yr$RCP.name <- car::recode(runs.yr$rcp, "'rcp45'='Low Emmissions'; 'rcp85'='High Emissions'")
 runs.yr$RCP.name <- factor(runs.yr$RCP.name, levels=c("Low Emmissions", "High Emissions"))
 runs.yr$loss.event.20 <- ifelse(runs.yr$agb.rel.diff<=-0.2, 1, 0)
@@ -40,6 +41,7 @@ for(i in 1:nrow(runs.yr)){
 
 runs.yr$crash <- ifelse(runs.yr$nonseq.loss.event.20==T, 1, 0)
 summary(runs.yr)
+runs.yr$density.tree.convert <- runs.yr$density.tree * 10000
 #runs.yr <- runs.yr[runs.yr$year>=2025,]
 
 #---------------------------------------------------#
@@ -133,12 +135,12 @@ for(RCP in unique(runs.yr$rcp)){
         
         if(!is.na(runs.yr[runs.yr$rcp == RCP & runs.yr$GCM == GCM & runs.yr$Management == MNG & runs.yr$year == (YR), "ind.crash.lag"])){
           temp.store <- runs.yr[runs.yr$rcp == RCP & runs.yr$GCM == GCM & runs.yr$Management == MNG & runs.yr$year == (YR),]
-          temp.store[temp.store$rcp == RCP & temp.store$GCM == GCM & temp.store$Management == MNG & temp.store$year == (YR), "ind.crash.lag"] <- "crash"
+          temp.store[temp.store$rcp == RCP & temp.store$GCM == GCM & temp.store$Management == MNG & temp.store$year == (YR), "ind.crash.lag"] <- "loss"
           temp.store[temp.store$rcp == RCP & temp.store$GCM == GCM & temp.store$Management == MNG & temp.store$year == (YR), "crash.year"] <- YR
           temp.store[temp.store$rcp == RCP & temp.store$GCM == GCM & temp.store$Management == MNG & temp.store$year == (YR), "group.crash.lag.check"] <- "Y"
           temp.df <- rbind(temp.df, temp.store)
         } else{
-          runs.yr[runs.yr$rcp == RCP & runs.yr$GCM == GCM & runs.yr$Management == MNG & runs.yr$year == (YR), "ind.crash.lag"] <- "crash"
+          runs.yr[runs.yr$rcp == RCP & runs.yr$GCM == GCM & runs.yr$Management == MNG & runs.yr$year == (YR), "ind.crash.lag"] <- "loss"
           runs.yr[runs.yr$rcp == RCP & runs.yr$GCM == GCM & runs.yr$Management == MNG & runs.yr$year == (YR), "crash.year"] <- YR
           runs.yr[runs.yr$rcp == RCP & runs.yr$GCM == GCM & runs.yr$Management == MNG & runs.yr$year == (YR), "group.crash.lag.check"] <- "Y"
         }
@@ -169,7 +171,7 @@ for(RCP in unique(runs.yr$rcp)){
         temp.df[temp.df$rcp == RCP & temp.df$GCM == GCM & temp.df$year == (YR-3), "group.crash.lag"] <- -3
         temp.df[temp.df$rcp == RCP & temp.df$GCM == GCM & temp.df$year == (YR-2), "group.crash.lag"] <- -2
         temp.df[temp.df$rcp == RCP & temp.df$GCM == GCM & temp.df$year == (YR-1), "group.crash.lag"] <- -1
-        temp.df[temp.df$rcp == RCP & temp.df$GCM == GCM & temp.df$year == YR, "group.crash.lag"] <- "crash"
+        temp.df[temp.df$rcp == RCP & temp.df$GCM == GCM & temp.df$year == YR, "group.crash.lag"] <- "loss"
         runs.fill <- rbind(runs.fill, temp.df)
         
       }
@@ -177,7 +179,7 @@ for(RCP in unique(runs.yr$rcp)){
   }
 }
 
-runs.fill$ind.crash.lag <- factor(runs.fill$ind.crash.lag, levels=c("-5", "-4", "-3", "-2", "-1", "crash"))
+runs.fill$ind.crash.lag <- factor(runs.fill$ind.crash.lag, levels=c("-5", "-4", "-3", "-2", "-1", "loss"))
 summary(runs.fill)
 
 time.weath.agg <- aggregate(cbind(diff.tair, rel.precip, rel.VPD)~ind.crash.lag, data = runs.fill[!is.na(runs.fill$ind.crash.lag),], FUN = mean, na.action = NULL)
@@ -200,27 +202,29 @@ raw.met.tair <- ggplot(data=runs.fill[!is.na(runs.fill$ind.crash.lag),], aes(x=i
   geom_errorbar(aes(color=Management), stat="summary", fun.y="sd", size=1.25, alpha=0.5) +
   geom_line(aes(color=Management), stat="summary", fun="mean", size=1.5) +
   geom_point(aes(color=Management), stat="summary", fun="mean", size=2) +
-  scale_color_manual(values=c("None"="#1f78b4", "Under"="#a6cee3", "Shelter"="#33a02c", "Gap"="#b2df8a")) +
-  theme_bw() + theme(axis.title.x=element_blank(), panel.spacing.y = unit(2, "lines"))
+  scale_color_manual(values=c("None"="#1f78b4", "Under"="#a6cee3", "Shelter"="#33a02c", "Group"="#b2df8a")) +
+  theme_bw() + theme(axis.title.x=element_blank(), panel.spacing.y = unit(2, "lines"))+
+  ylab("Temperature (C)")
 
 raw.met.precip <- ggplot(data=runs.fill[!is.na(runs.fill$ind.crash.lag),], aes(x=ind.crash.lag, y=rel.precip, group=Management), position=dodge) +
   geom_errorbar(aes(color=Management), stat="summary", fun.y="sd", size=1.25, alpha=0.5) +
   geom_line(aes(color=Management), stat="summary", fun="mean", size=1.5) +
   geom_point(aes(color=Management), stat="summary", fun="mean", size=2) +
-  scale_color_manual(values=c("None"="#1f78b4", "Under"="#a6cee3", "Shelter"="#33a02c", "Gap"="#b2df8a")) +
-  theme_bw() + theme(axis.title.x=element_blank(), panel.spacing.y = unit(2, "lines"))
+  scale_color_manual(values=c("None"="#1f78b4", "Under"="#a6cee3", "Shelter"="#33a02c", "Group"="#b2df8a")) +
+  theme_bw() + theme(axis.title.x=element_blank(), panel.spacing.y = unit(2, "lines"))+
+  ylab("Total precip (m)")
 
 raw.met.vpd <- ggplot(data=runs.fill[!is.na(runs.fill$ind.crash.lag),], aes(x=ind.crash.lag, y=rel.VPD, group=Management), position=dodge) +
   geom_errorbar(aes(color=Management), stat="summary", fun.y="sd", size=1.25, alpha=0.5) +
   geom_line(aes(color=Management), stat="summary", fun="mean", size=1.5) +
   geom_point(aes(color=Management), stat="summary", fun="mean", size=2) +
-  scale_color_manual(values=c("None"="#1f78b4", "Under"="#a6cee3", "Shelter"="#33a02c", "Gap"="#b2df8a")) +
-  theme_bw() + theme(axis.title.x=element_blank(), panel.spacing.y = unit(2, "lines"))
+  scale_color_manual(values=c("None"="#1f78b4", "Under"="#a6cee3", "Shelter"="#33a02c", "Group"="#b2df8a")) +
+  theme_bw() + theme(axis.title.x=element_blank(), panel.spacing.y = unit(2, "lines"))+
+  ylab("VPD (kPa)")
 
 png(file.path(path.figures, "SEA_RelWeather_TimeMgmt_RawDat.png"), width=12, height=8, units="in", res=220)
-cowplot::plot_grid(raw.met.tair, raw.met.precip, raw.met.vpd, ncol=2)
+  cowplot::plot_grid(raw.met.tair, raw.met.precip, raw.met.vpd, ncol=2)
 dev.off()
-
 
 
 relmet.var <- c("rel.precip", "diff.tair", "rel.VPD")
@@ -249,8 +253,8 @@ for(COL in relmet.var){
   
 
   df.plot <- data.frame(summary(mod.plot)$tTable)
-  df.plot$time <- c("-5", "-4", "-3", "-2", "-1", "crash")
-  df.plot$Management <- rep(c("None", "Under", "Shelter", "Gap"), each=6)
+  df.plot$time <- c("-5", "-4", "-3", "-2", "-1", "loss")
+  df.plot$Management <- rep(c("None", "Under", "Shelter", "Group"), each=6)
   df.plot$VAR <- COL
   rownames(df.plot) <- NULL
   
@@ -262,7 +266,7 @@ for(COL in relmet.var){
   mod.time <- nlme::lme(eval(substitute(j ~ relevel(as.factor(ind.crash.lag), "-5")-1, list(j = as.name(COL)))), random=list(rcp = ~1, GCM =~1, Management=~1), data = runs.fill[!is.na(runs.fill$ind.crash.lag),], na.action = na.omit)
   
   df.time <- data.frame(summary(mod.time)$tTable)
-  df.time$comp <- c("-5", "-4", "-3", "-2", "-1", "crash")
+  df.time$comp <- c("-5", "-4", "-3", "-2", "-1", "loss")
   df.time$VAR <- COL
   rownames(df.time) <- NULL
   
@@ -273,7 +277,7 @@ for(COL in relmet.var){
   
   
   df.mgmt <- data.frame(summary(mod.mgmt)$tTable)
-  df.mgmt$comp <- c("None", "Under", "Shelter", "Gap")
+  df.mgmt$comp <- c("None", "Under", "Shelter", "Group")
   df.mgmt$VAR <- COL
   rownames(df.mgmt) <- NULL
   
@@ -311,13 +315,13 @@ write.csv(df.mgmt.relmetxind, file.path(path.google, "Drought and heat analysis"
 
 
 
-df.plot.relmetxind$time <- factor(df.plot.relmetxind$time, levels=c("-5", "-4", "-3", "-2", "-1", "crash"))
+df.plot.relmetxind$time <- factor(df.plot.relmetxind$time, levels=c("-5", "-4", "-3", "-2", "-1", "loss"))
 ggplot(data=df.plot.relmetxind, aes(x=time, y=Value, group=Management, color=Management)) +
   facet_wrap(~VAR, scales="free_y", ncol=2) +
   geom_line(size=1.5) + 
   geom_point(size=2) +
   geom_errorbar(aes(x=time, ymin=Value-Std.Error, ymax=Value+Std.Error, group=Management, color=Management), size=1.5, alpha=0.5) +
-  scale_color_manual(values=c("None"="#1f78b4", "Under"="#a6cee3", "Shelter"="#33a02c", "Gap"="#b2df8a")) +
+  scale_color_manual(values=c("None"="#1f78b4", "Under"="#a6cee3", "Shelter"="#33a02c", "Group"="#b2df8a")) +
   theme_bw() + theme(axis.title.x=element_blank(), panel.spacing.y = unit(2, "lines"))
 
 
@@ -334,7 +338,7 @@ ggplot(data=df.plot.relmetxind, aes(x=time, y=Value, group=Management, color=Man
 #   
 #   # Updating this to be compared to year 5; then need to add the value to the others to get the absolute magnitudes down the road
 #   mod.lag <- nlme::lme(eval(substitute(j ~ relevel(as.factor(ind.crash.lag), "-5"), list(j = as.name(COL)))), random=list(rcp = ~1, GCM =~1), data = runs.fill[!is.na(runs.fill$ind.crash.lag),], na.action = na.omit)
-#   # mod.lag2 <- nlme::lme(eval(substitute(j ~ relevel(as.factor(ind.crash.lag), "crash")-1, list(j = as.name(COL)))), random=list(rcp = ~1, GCM =~1), data = runs.fill[!is.na(runs.fill$ind.crash.lag),], na.action = na.omit)
+#   # mod.lag2 <- nlme::lme(eval(substitute(j ~ relevel(as.factor(ind.crash.lag), "loss")-1, list(j = as.name(COL)))), random=list(rcp = ~1, GCM =~1), data = runs.fill[!is.na(runs.fill$ind.crash.lag),], na.action = na.omit)
 #   # output2 <- summary(mod.lag2)
 #   
 #   output <- summary(mod.lag)
@@ -346,7 +350,7 @@ ggplot(data=df.plot.relmetxind, aes(x=time, y=Value, group=Management, color=Man
 #   lag.list.rel[[paste(COL)]]$t.stat <- output$tTable[,"t-value"]
 #   lag.list.rel[[paste(COL)]]$p.val <- output$tTable[,"p-value"]
 #   temp.lag.rel <- dplyr::bind_rows(lag.list.rel)
-#   temp.lag.rel$lag <- c("-5", -1, -2, -3, -4, "crash")
+#   temp.lag.rel$lag <- c("-5", -1, -2, -3, -4, "loss")
 #   
 #   df.lag.rel <- rbind(df.lag.rel, temp.lag.rel)
 #   
@@ -362,7 +366,7 @@ ggplot(data=df.plot.relmetxind, aes(x=time, y=Value, group=Management, color=Man
 #   theme(panel.spacing = unit(0, "lines"),
 #         panel.grid = element_blank(),
 #         panel.background=element_rect(fill=NA, color="black"))+
-#   scale_x_discrete(limits = factor(c(-5, -4, -3, -2, -1, "crash")))+
+#   scale_x_discrete(limits = factor(c(-5, -4, -3, -2, -1, "loss")))+
 #   ggtitle("Relative weather before crashes")
 # 
 # png(paste0(path.figures, "RelWeather_before_crash_hist.png"), width=12, height=8, units="in", res=220)
@@ -374,12 +378,12 @@ ggplot(data=df.plot.relmetxind, aes(x=time, y=Value, group=Management, color=Man
 # ind.crash.lag = time lag for individual management which crashed
 # We exclude the year of crash
 #-----------------------------------------------------#
-struc.var <- c("agb", "density.tree", "tree.dbh.mean", "tree.dbh.sd")
+struc.var <- c("agb", "density.tree.convert", "tree.dbh.mean", "tree.dbh.sd")
 df.lag.strucxind <- data.frame()
 df.ano.strucxind <- data.frame()
 for(COL in struc.var){
   
-  mod.lag <- nlme::lme(eval(substitute(j ~ relevel(as.factor(ind.crash.lag), "-5")*relevel(Management, "None"), list(j = as.name(COL)))), random=list(rcp = ~1, GCM =~1), data = runs.fill[!is.na(runs.fill$ind.crash.lag) & runs.fill$ind.crash.lag!="crash",], na.action = na.omit)
+  mod.lag <- nlme::lme(eval(substitute(j ~ relevel(as.factor(ind.crash.lag), "-5")*relevel(Management, "None"), list(j = as.name(COL)))), random=list(rcp = ~1, GCM =~1), data = runs.fill[!is.na(runs.fill$ind.crash.lag) & runs.fill$ind.crash.lag!="loss",], na.action = na.omit)
   anova(mod.lag)
   
   df.ano <- data.frame(anova(mod.lag))
@@ -417,10 +421,10 @@ write.csv(df.ano.strucxind, file.path(path.google, "Drought and heat analysis", 
 # for(COL in struc.var){
 #   
 #   # This one will get statistical difference relative to the start
-#   mod.lag <- nlme::lme(eval(substitute(j ~ relevel(as.factor(ind.crash.lag), "-5"), list(j = as.name(COL)))), random=list(rcp = ~1, GCM =~1), data = runs.fill[!is.na(runs.fill$ind.crash.lag) & runs.fill$ind.crash.lag != "crash",], na.action = na.omit)
+#   mod.lag <- nlme::lme(eval(substitute(j ~ relevel(as.factor(ind.crash.lag), "-5"), list(j = as.name(COL)))), random=list(rcp = ~1, GCM =~1), data = runs.fill[!is.na(runs.fill$ind.crash.lag) & runs.fill$ind.crash.lag != "loss",], na.action = na.omit)
 #   
 #   # # This one will show means etc. & get assess stat sig as difference from 0, which isn't inherently meaningful because everything should be non-0; the way to show this would be breakign down by the anovas above
-#   # mod.lag2 <- nlme::lme(eval(substitute(j ~ relevel(as.factor(ind.crash.lag), "-5")-1, list(j = as.name(COL)))), random=list(rcp = ~1, GCM =~1), data = runs.fill[!is.na(runs.fill$ind.crash.lag) & runs.fill$ind.crash.lag != "crash",], na.action = na.omit)
+#   # mod.lag2 <- nlme::lme(eval(substitute(j ~ relevel(as.factor(ind.crash.lag), "-5")-1, list(j = as.name(COL)))), random=list(rcp = ~1, GCM =~1), data = runs.fill[!is.na(runs.fill$ind.crash.lag) & runs.fill$ind.crash.lag != "loss",], na.action = na.omit)
 #   # output2 <- summary(mod.lag2)
 #   
 #   output <- summary(mod.lag)
@@ -466,7 +470,7 @@ write.csv(df.ano.strucxind, file.path(path.google, "Drought and heat analysis", 
 # for(COL in struc.var){
 #   
 #   # Checkign to see if there's anything if we move MGMT to FIXED
-#   mod.lag <- nlme::lme(eval(substitute(j ~ relevel(as.factor(group.crash.lag), "-1")*relevel(as.factor(group.crash.lag.check), "N")*relevel(Management, "None"), list(j = as.name(COL)))), random=list(rcp = ~1, GCM =~1), data = runs.fill[!is.na(runs.fill$group.crash.lag) & runs.fill$group.crash.lag!="crash",], na.action = na.omit)
+#   mod.lag <- nlme::lme(eval(substitute(j ~ relevel(as.factor(group.crash.lag), "-1")*relevel(as.factor(group.crash.lag.check), "N")*relevel(Management, "None"), list(j = as.name(COL)))), random=list(rcp = ~1, GCM =~1), data = runs.fill[!is.na(runs.fill$group.crash.lag) & runs.fill$group.crash.lag!="loss",], na.action = na.omit)
 #   anova(mod.lag)
 #   
 #   df.ano <- anova(mod.lag)
@@ -485,7 +489,7 @@ write.csv(df.ano.strucxind, file.path(path.google, "Drought and heat analysis", 
 #   temp.lag.strucxcrashxmng <- dplyr::bind_rows(lag.list.strucxcrashxmng)
 #   temp.lag.strucxcrashxmng$lag <- c(NA, -2,-3,-4,-5, NA, NA, NA, NA, rep(unique(-2:-5), times = 4), NA, NA, NA, rep(unique(-2:-5), times = 3))
 #   temp.lag.strucxcrashxmng$crash <- c(NA, NA, NA, NA, NA, "Y", NA, NA, NA, "Y", "Y", "Y", "Y", rep(c(NA), each = 12), rep(c("Y"), each = 15))
-#   temp.lag.strucxcrashxmng$Management <- c(NA, NA, NA, NA, NA, NA, "Under", "Shelter", "Gap", NA, NA, NA, NA, rep(c("Under", "Shelter", "Gap"), each = 4), "Under", "Shelter", "Gap", rep(c("Under", "Shelter", "Gap"), each = 4))
+#   temp.lag.strucxcrashxmng$Management <- c(NA, NA, NA, NA, NA, NA, "Under", "Shelter", "Group", NA, NA, NA, NA, rep(c("Under", "Shelter", "Group"), each = 4), "Under", "Shelter", "Group", rep(c("Under", "Shelter", "Group"), each = 4))
 #   
 #   df.lag.strucxcrashxmng <- rbind(df.lag.strucxcrashxmng, temp.lag.strucxcrashxmng)
 #   df.ano.strucxcrashxmng <- rbind(df.ano.strucxcrashxmng, df.ano)
@@ -503,7 +507,7 @@ write.csv(df.ano.strucxind, file.path(path.google, "Drought and heat analysis", 
 # #IMPORTANT!!!!! Rounding for easy reading. This should not be how the values are reported in the end
 # df.ano.strucxcrashxmng$`p-value` <- round(df.ano.strucxcrashxmng$`p-value`, 5)
 # 
-# #df.ano.strucx <- df.ano.strucxcrashxmng[df.ano.strucxcrashxmng$VAR== "density.tree",]
+# #df.ano.strucx <- df.ano.strucxcrashxmng[df.ano.strucxcrashxmng$VAR== "density.tree.convert",]
 # 
 # write.csv(df.ano.strucxcrashxmng, file.path(path.google, "processed_data/strucxcrashxmng_anova.csv"), row.names = F)
 # 
@@ -541,7 +545,7 @@ write.csv(df.ano.strucxind, file.path(path.google, "Drought and heat analysis", 
 # dat.strucxmng$sig <- as.factor(dat.strucxmng$sig)
 # summary(dat.strucxmng)
 # 
-# dat.strucxmng$VAR <- car::recode(dat.strucxmng$VAR, "'agb'='AGB'; 'density.tree'='Tree Density'; 
+# dat.strucxmng$VAR <- car::recode(dat.strucxmng$VAR, "'agb'='AGB'; 'density.tree.convert'='Tree Density'; 
 #                              'tree.dbh.mean'='Mean DBH'; 'tree.dbh.sd'='SD of DBH'")
 # 
 # plot.strucxmng <- ggplot(data=dat.strucxmng[!is.na(dat.strucxmng$lag),]) +
